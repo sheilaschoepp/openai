d71d8c95fc0007a1fe48616c8154e1ae591cd7a1
Sheila Schoepp <sschoepp@ualberta.ca>
2021-05-18 14:24:45 -0600

    us

--- scripts/melco1.sh
+++ scripts/melco1.sh
@@ -2,17 +2,50 @@
 
 SAC_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/sacv2_n_controller.py"
 
-for s in {0..4}
+for ps in 2
 do
-  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=0 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c --resumable -s $s -t 5000000"
+  for s in {0..4}
+  do
+    tmux new-session -d -s sac$ps$s "CUDA_VISIBLE_DEVICES=0 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -s $s -t 5000000"
+  done
 done
 
-for s in {5..9}
+for ps in 3
 do
-  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=1 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c --resumable -s $s -t 5000000"
+  for s in {0..4}
+  do
+    tmux new-session -d -s sac$ps$s "CUDA_VISIBLE_DEVICES=1 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -s $s -t 5000000"
+  done
 done
 
-for s in {10..14}
+for ps in 4
 do
-  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=3 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c --resumable -s $s -t 5000000"
+  for s in {0..4}
+  do
+    tmux new-session -d -s sac$ps$s "CUDA_VISIBLE_DEVICES=3 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -s $s -t 5000000"
+  done
+done
+
+for ps in 5
+do
+  for s in {0..4}
+  do
+    tmux new-session -d -s sac$ps$s "CUDA_VISIBLE_DEVICES=4 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -s $s -t 5000000"
+  done
+done
+
+for ps in 6
+do
+  for s in {0..4}
+  do
+    tmux new-session -d -s sac$ps$s "CUDA_VISIBLE_DEVICES=6 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -s $s -t 5000000"
+  done
+done
+
+for ps in 7
+do
+  for s in {0..4}
+  do
+    tmux new-session -d -s sac$ps$s "CUDA_VISIBLE_DEVICES=7 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -s $s -t 5000000"
+  done
 done
--- scripts/melco2.sh
+++ scripts/melco2.sh
@@ -6,19 +6,24 @@ for ps in 0 1 2 3 4 5 6 7
 do
   for s in {0..4}
   do
-    label=$((s))
-    tmux new-session -d -s ppo$label "python $PPO_N_CONTROLLER_ABSOLUTE_PATH -lrd -s $s -t 100000000"
+    tmux new-session -d -s ppo$ps$s "python $PPO_N_CONTROLLER_ABSOLUTE_PATH -lrd -s $s -t 100000000"
   done
 done
 
 SAC_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/sacv2_n_controller.py"
 
-for s in {15..19}
+for ps in 0
 do
-  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=0 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -s $s -t 5000000"
+  for s in {0..4}
+  do
+    tmux new-session -d -s sac$ps$s "CUDA_VISIBLE_DEVICES=0 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -s $s -t 5000000"
+  done
 done
 
-for s in {20..24}
+for ps in 1
 do
-  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=1 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -s $s -t 5000000"
+  for s in {0..4}
+  do
+    tmux new-session -d -s sac$ps$s "CUDA_VISIBLE_DEVICES=1 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -s $s -t 5000000"
+  done
 done

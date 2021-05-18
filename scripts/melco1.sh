51052c142320923c633ede5a96835c454c0b1325
Sheila Schoepp <sschoepp@ualberta.ca>
2021-05-18 14:16:43 -0600

    us

--- scripts/melco1.sh
+++ scripts/melco1.sh
@@ -4,15 +4,15 @@ SAC_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/
 
 for s in {0..4}
 do
-  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=0 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c --resumable -s $s"
+  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=0 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c --resumable -s $s -t 5000000"
 done
 
 for s in {5..9}
 do
-  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=1 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c --resumable -s $s"
+  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=1 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c --resumable -s $s -t 5000000"
 done
 
 for s in {10..14}
 do
-  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=3 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c --resumable -s $s"
+  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=3 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c --resumable -s $s -t 5000000"
 done
--- scripts/melco2.sh
+++ scripts/melco2.sh
@@ -2,11 +2,23 @@
 
 PPO_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/ppov2_n_controller.py"
 
-for ps in 10 11 # 0 - 9
+for ps in 0 1 2 3 4 5 6 7
 do
   for s in {0..4}
   do
     label=$((s))
-    tmux new-session -d -s ppo$label "python $PPO_N_CONTROLLER_ABSOLUTE_PATH -lrd --resumable -s $s -t 800000 -ef 200000"
+    tmux new-session -d -s ppo$label "python $PPO_N_CONTROLLER_ABSOLUTE_PATH -lrd --resumable -s $s -t 100000000"
   done
+done
+
+SAC_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/sacv2_n_controller.py"
+
+for s in {15..19}
+do
+  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=0 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c --resumable -s $s -t 5000000"
+done
+
+for s in {20..24}
+do
+  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=1 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c --resumable -s $s -t 5000000"
 done

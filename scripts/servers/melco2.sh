cb9e52dd413c3c68fd102e91646ab510dcffbb4c
Sheila Schoepp <sschoepp@ualberta.ca>
2021-05-22 23:24:58 -0600

    us

--- scripts/servers/melco2.sh
+++ scripts/servers/melco2.sh
@@ -28,19 +28,19 @@
 #  done
 #done
 
-#PPO_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/ppov2_n_controller.py"
-#
-#for ps in 0 1 2 3 4 5 6 7
-#do
-#  for s in {0..4}
-#  do
-#    tmux new-session -d -s ppo$ps$s "python $PPO_N_CONTROLLER_ABSOLUTE_PATH -lrd -s $s -t 100000000 -ps -pss $ps"
-#  done
-#done
+PPO_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/ppov2_n_controller.py"
+
+for ps in {8..12}
+do
+  for s in {0..4}
+  do
+    tmux new-session -d -s ppo$ps$s "python $PPO_N_CONTROLLER_ABSOLUTE_PATH -lrd -s $s -t 100000000 -ps -pss $ps"
+  done
+done
 
 SAC_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/sacv2_n_controller.py"
 
-for ps in 8
+for ps in 21
 do
   for s in {0..4}
   do
@@ -48,7 +48,7 @@ do
   done
 done
 
-for ps in 9
+for ps in 22
 do
   for s in {0..4}
   do

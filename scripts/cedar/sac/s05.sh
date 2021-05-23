99b173fda3164767b41e3f39cc99885052f3c404
Sheila Schoepp <sschoepp@ualberta.ca>
2021-05-23 09:14:39 -0600

    us

--- scripts/cedar/sac/s05.sh
+++ scripts/cedar/sac/s05.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks-per-node=32
 #SBATCH --mem=187G
 #SBATCH --time=4-00:00
-#SBATCH --job-name=sac_25_29
+#SBATCH --job-name=sac_45_49
 #SBATCH --output=%x-%j.out
 #SBATCH --mail-user=sschoepp@ualberta.ca
 #SBATCH --mail-type=BEGIN

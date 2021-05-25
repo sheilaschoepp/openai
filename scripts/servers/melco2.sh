529a42a00d839841b65b9c452dc6189d84f7a5b6
Sheila Schoepp <sschoepp@ualberta.ca>
2021-05-25 12:17:48 -0600

    us

--- scripts/servers/melco2.sh
+++ scripts/servers/melco2.sh
@@ -30,7 +30,7 @@
 
 PPO_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/ppov2_n_controller.py"
 
-for ps in 8 9 10 11 12 13
+for ps in 8 9 10 11 12 13 14
 do
   for s in {0..4}
   do

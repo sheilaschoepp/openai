59bb51d6f441114cdb083a5582bdd4fecf71ad86
Sheila Schoepp <sschoepp@ualberta.ca>
2021-05-31 11:59:29 -0600

    us

--- scripts/cedar/sac/mehran/stest.sh
+++ scripts/cedar/sac/mehran/stest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:v100l:1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem=10G
+#SBATCH --gres=gpu:v100l:4
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=187G
 #SBATCH --time=0-1:00
 #SBATCH --job-name=sac_test
 #SBATCH --output=%x-%j.out
--- scripts/cedar/sac/sheila/stest.sh
+++ scripts/cedar/sac/sheila/stest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:v100l:1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem=10G
-#SBATCH --time=0-0:20
+#SBATCH --gres=gpu:v100l:4
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=187G
+#SBATCH --time=0-1:00
 #SBATCH --job-name=sac_test
 #SBATCH --output=%x-%j.out
 #SBATCH --mail-user=sschoepp@ualberta.ca

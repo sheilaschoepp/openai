6ba1a060e19be34c7332113d40ec158371c47870
Sheila Schoepp <sschoepp@ualberta.ca>
2021-05-31 11:25:16 -0600

    updated stest

--- scripts/cedar/sac/sheila/stest.sh
+++ scripts/cedar/sac/sheila/stest.sh
@@ -3,7 +3,7 @@
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:v100l:1
 #SBATCH --ntasks-per-node=1
-#SBATCH --mem=187G
+#SBATCH --mem=10G
 #SBATCH --time=0-0:20
 #SBATCH --job-name=sac_test
 #SBATCH --output=%x-%j.out

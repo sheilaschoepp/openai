296f66869c374b846c918e137ce2c2ded225337d
Sheila Schoepp <sschoepp@ualberta.ca>
2021-05-31 18:06:00 -0600

    removed scripts

--- scripts/setup/cc.sh
+++
@@ -1,5 +0,0 @@
-module load python/3.7
-virtualenv --no-download ~/envs/openai
-pip install --no-index --upgrade pip
-pip install numpy Cython pandas termcolor matplotlib cffi imageio pycparser lockfile imageio torch torchvision --no-index
-pip install gym[all]

198364c6791b009586d3fc3bc9df91e144ef647e
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-03 02:54:40 +0000

    us

---
+++ scripts/setup/s8.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:v100l:4
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=187G
+#SBATCH --time=7-00:00
+#SBATCH --job-name=sac_8
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=sschoepp@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < s8.txt

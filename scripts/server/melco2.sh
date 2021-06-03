f07b078a659d854ca58f39288bdb8b72ae61ba88
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-03 17:25:11 -0600

    us

--- scripts/server/melco2.sh
+++ scripts/server/melco2.sh
@@ -2,14 +2,34 @@
 
 PPO_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/ppov2_n_controller.py"
 
-for s in {0..4}
+for s in 0
 do
   tmux new-session -d -s ppo$s "python $PPO_N_CONTROLLER_ABSOLUTE_PATH -e FetchPickAndPlaceEnv-v0 -lrd --resumable -s $s -t 100000000"
 done
 
+for s in 0
+do
+  tmux new-session -d -s ppo$s "python $PPO_N_CONTROLLER_ABSOLUTE_PATH -e FetchPickAndPlaceEnv-v1 -lrd --resumable -s $s -t 100000000"
+done
+
+for s in 0
+do
+  tmux new-session -d -s ppo$s "python $PPO_N_CONTROLLER_ABSOLUTE_PATH -e FetchPickAndPlaceEnv-v2 -lrd --resumable -s $s -t 100000000"
+done
+
 SAC_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/sacv2_n_controller.py"
 
-for s in {0..4}
+for s in 0
 do
   tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=1 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -e FetchPickAndPlaceEnv-v0 --resumable -s $s -t 5000000"
+done
+
+for s in 0
+do
+  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=1 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -e FetchPickAndPlaceEnv-v1 --resumable -s $s -t 5000000"
+done
+
+for s in 0
+do
+  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=1 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -e FetchPickAndPlaceEnv-v2 --resumable -s $s -t 5000000"
 done

224d34835303b8602c2b3a4afc977c3e285c2a2e
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-03 18:30:10 -0600

    us

--- scripts/server/melco2.sh
+++ scripts/server/melco2.sh
@@ -19,7 +19,7 @@ done
 
 for s in 3
 do
-  tmux new-session -d -s ppo$s "python $PPO_N_CONTROLLER_ABSOLUTE_PATH -e FetchReach-v1 -lrd --resumable -s $s -t 100000000 --lr 0.0003 --policy_entropy_coef 0.0 -ns 2000 -mbs 50 --epsilon 0.2"
+  tmux new-session -d -s ppo$s "python $PPO_N_CONTROLLER_ABSOLUTE_PATH -e FetchReach-v1 -lrd --resumable -s $s -t 100000000 --lr 0.0003 --policy_entropy_coef 0.0 -ns 2000 -mbs 50 --epsilon 0.2; test.py"
 done
 
 SAC_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/sacv2_n_controller.py"
@@ -41,5 +41,5 @@ done
 
 for s in 3
 do
-  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=1 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -e FetchReach-v1 -ef 10000 --resumable -s $s -t 5000000"
+  tmux new-session -d -s sac$s "CUDA_VISIBLE_DEVICES=1 python $SAC_CONTROLLER_ABSOLUTE_PATH -a -c -e FetchReach-v1 -ef 10000 --resumable -s $s -t 5000000; python test.py"
 done

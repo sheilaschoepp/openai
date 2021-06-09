d63984c0471c1eecbe7294514852473428728f57
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-09 09:57:47 -0600

    us

--- scripts/beluga/sac/sheila/stest.sh
+++ scripts/beluga/sac/sheila/stest.sh
@@ -4,7 +4,7 @@
 #SBATCH --gres=gpu:v100:4
 #SBATCH --ntasks-per-node=20
 #SBATCH --mem=191000M
-#SBATCH --time=0-0:30
+#SBATCH --time=0-0:10
 #SBATCH --job-name=sac_test
 #SBATCH --output=%x-%j.out
 #SBATCH --mail-user=sschoepp@ualberta.ca

f3e37c11184e7afb6b4b708b6462226a0d5f90c3
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-09 09:55:14 -0600

    us

---
+++ scripts/beluga/sac/sheila/s46.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:v100:4
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=191000M
+#SBATCH --time=7-00:00
+#SBATCH --job-name=sac_46
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=sschoepp@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < s46.txt

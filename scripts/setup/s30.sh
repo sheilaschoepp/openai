8a0534312f1dec484998786434c6b98f3749227b
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-16 22:59:35 +0000

    us

---
+++ scripts/setup/s30.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=30
+#SBATCH --mem=125G
+#SBATCH --time=12-00:00
+#SBATCH --job-name=ppo_30
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=sschoepp@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < s30.txt

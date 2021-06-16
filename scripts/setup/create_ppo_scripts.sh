6293a45a61217b51d8daee5f66d6399fcbb9f08c
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-16 16:45:30 -0600

    us

--- scripts/setup/create_ppo_scripts.sh
+++ scripts/setup/create_ppo_scripts.sh
@@ -72,36 +72,36 @@ parallel < s$i.txt" >> s"$i".sh
 
   j=$((i+1))
   k=$((i+2))
-  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 0 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 1 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 2 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 3 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 4 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 5 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 6 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 7 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 8 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 9 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 0 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 1 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 2 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 3 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 4 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 5 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 6 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 7 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 8 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 9 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 0 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 1 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 2 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 3 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 4 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 5 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 6 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 7 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 8 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 9 -tl 12 -ps -pss $k" >> s"$i".txt
+  echo "python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 0 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 1 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 2 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 3 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 4 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 5 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 6 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 7 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 8 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 9 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 0 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 1 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 2 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 3 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 4 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 5 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 6 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 7 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 8 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 9 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 0 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 1 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 2 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 3 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 4 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 5 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 6 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 7 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 8 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 9 -tl 12 -ps -pss $k" >> s"$i".txt
 done
 
 for i in 99
@@ -123,14 +123,14 @@ do
 
 parallel < s$i.txt" >> s"$i".sh
 
-  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 0 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 1 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 2 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 3 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 4 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 5 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 6 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 7 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 8 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 9 -tl 12 -ps -pss $i" >> s"$i".txt
+  echo "python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 0 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 1 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 2 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 3 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 4 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 5 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 6 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 7 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 8 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 9 -tl 12 -ps -pss $i" >> s"$i".txt
 done

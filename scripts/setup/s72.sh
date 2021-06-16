ef9d4888a75594c2a0deafd0d346e8f8547840e2
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-16 23:46:50 +0000

    us

---
+++ scripts/setup/s72.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=30
+#SBATCH --mem=125G
+#SBATCH --time=14-00:00
+#SBATCH --job-name=ppo_72
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=taghianj@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < s72.txt
---
+++ scripts/setup/s99.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --mem=42G
+#SBATCH --time=14-00:00
+#SBATCH --job-name=ppo_99
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=taghianj@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < s99.txt

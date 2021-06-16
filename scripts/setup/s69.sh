cb78371fca6ff3ff64c7b2ed50ddfd0b5b3b6566
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-16 22:56:58 +0000

    us

--- scripts/setup/s69.sh
+++ scripts/setup/s69.sh
@@ -14,19 +14,3 @@
 #SBATCH --mail-type=ALL
 
 parallel < s69.txt
-#!/bin/bash
-
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=30
-#SBATCH --mem=125G
-#SBATCH --time=12-00:00
-#SBATCH --job-name=ppo_69
-#SBATCH --output=%x-%j.out
-#SBATCH --mail-user=taghianj@ualberta.ca
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
-#SBATCH --mail-type=REQUEUE
-#SBATCH --mail-type=ALL
-
-parallel < s69.txt

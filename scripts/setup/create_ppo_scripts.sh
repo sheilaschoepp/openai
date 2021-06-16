0fc9bd6ef4b9724ee802d922ca3e9a59da4c8ace
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-16 16:42:56 -0600

    us

--- scripts/setup/create_ppo_scripts.sh
+++ scripts/setup/create_ppo_scripts.sh
@@ -1,4 +1,4 @@
-for i in {0..48..2}
+for i in {0..48..3}
 do
   echo "#!/bin/bash
 
@@ -16,11 +16,9 @@ do
 #SBATCH --mail-type=ALL
 
 parallel < s$i.txt" >> s"$i".sh
-done
 
-for i in {0..48..2}
-do
   j=$((i+1))
+  k=$((i+2))
   echo "python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 0 -tl 12 -ps -pss $i
 python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 1 -tl 12 -ps -pss $i
 python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 2 -tl 12 -ps -pss $i
@@ -53,7 +51,7 @@ python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lr
 python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 9 -tl 12 -ps -pss $k" >> s"$i".txt
 done
 
-for i in {50..98..2}
+for i in {51..96..2}
 do
   echo "#!/bin/bash
 
@@ -71,11 +69,9 @@ do
 #SBATCH --mail-type=ALL
 
 parallel < s$i.txt" >> s"$i".sh
-done
 
-for i in {0..48..2}
-do
   j=$((i+1))
+  k=$((i+2))
   echo "python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 0 -tl 12 -ps -pss $i
 python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 1 -tl 12 -ps -pss $i
 python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 2 -tl 12 -ps -pss $i
@@ -106,4 +102,35 @@ python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lr
 python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 7 -tl 12 -ps -pss $k
 python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 8 -tl 12 -ps -pss $k
 python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 9 -tl 12 -ps -pss $k" >> s"$i".txt
+done
+
+for i in 99
+do
+  echo "#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --mem=42G
+#SBATCH --time=12-00:00
+#SBATCH --job-name=ppo_$i
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=taghianj@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < s$i.txt" >> s"$i".sh
+
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 0 -tl 12 -ps -pss $i
+python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 1 -tl 12 -ps -pss $i
+python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 2 -tl 12 -ps -pss $i
+python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 3 -tl 12 -ps -pss $i
+python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 4 -tl 12 -ps -pss $i
+python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 5 -tl 12 -ps -pss $i
+python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 6 -tl 12 -ps -pss $i
+python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 7 -tl 12 -ps -pss $i
+python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 8 -tl 12 -ps -pss $i
+python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_n_controller.py -lrd -s 9 -tl 12 -ps -pss $i" >> s"$i".txt
 done

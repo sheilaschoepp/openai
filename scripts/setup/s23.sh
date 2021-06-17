658920a95b963ee458be092193ade3cd84961f5d
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-17 03:13:09 +0000

    us

---
+++ scripts/setup/s23.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=30
+#SBATCH --mem=187G
+#SBATCH --time=12-00:00
+#SBATCH --job-name=sac_23
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=sschoepp@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < s23.txt

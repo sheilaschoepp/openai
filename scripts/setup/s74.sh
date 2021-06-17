a79b7499b45226b0f14bfb89e23f4fd1ebe004e3
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-17 14:49:13 +0000

    us

---
+++ scripts/setup/s74.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=30
+#SBATCH --mem=187G
+#SBATCH --time=12-00:00
+#SBATCH --job-name=sac_74
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=sschoepp@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < s74.txt

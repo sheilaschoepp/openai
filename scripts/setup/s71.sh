e61a9863621a057d9be1bf15e4b751f3455a259a
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-17 03:22:23 +0000

    us

---
+++ scripts/setup/s71.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=30
+#SBATCH --mem=187G
+#SBATCH --time=12-00:00
+#SBATCH --job-name=sac_71
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=taghianj@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < s71.txt

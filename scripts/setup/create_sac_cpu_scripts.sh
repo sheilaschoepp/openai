6489dcf3dc0994f40ce50a88cc79feccc398dfe9
Sheila Schoepp <sschoepp@ualberta.ca>
2021-06-16 21:21:31 -0600

    us

--- scripts/setup/create_sac_cpu_scripts.sh
+++ scripts/setup/create_sac_cpu_scripts.sh
@@ -1,4 +1,57 @@
-for i in {2..23..3}
+#for i in {2..23..3}
+#do
+#  echo "#!/bin/bash
+#
+##SBATCH --nodes=1
+##SBATCH --ntasks-per-node=30
+##SBATCH --mem=187G
+##SBATCH --time=12-00:00
+##SBATCH --job-name=sac_$i
+##SBATCH --output=%x-%j.out
+##SBATCH --mail-user=sschoepp@ualberta.ca
+##SBATCH --mail-type=BEGIN
+##SBATCH --mail-type=END
+##SBATCH --mail-type=FAIL
+##SBATCH --mail-type=REQUEUE
+##SBATCH --mail-type=ALL
+#
+#parallel < s$i.txt" >> s"$i".sh
+#
+#  j=$((i+1))
+#  k=$((i+2))
+#  echo "python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 0 -tl 12 -ps -pss $i
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 1 -tl 12 -ps -pss $i
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 2 -tl 12 -ps -pss $i
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 3 -tl 12 -ps -pss $i
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 4 -tl 12 -ps -pss $i
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 5 -tl 12 -ps -pss $i
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 6 -tl 12 -ps -pss $i
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 7 -tl 12 -ps -pss $i
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 8 -tl 12 -ps -pss $i
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 9 -tl 12 -ps -pss $i
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 0 -tl 12 -ps -pss $j
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 1 -tl 12 -ps -pss $j
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 2 -tl 12 -ps -pss $j
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 3 -tl 12 -ps -pss $j
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 4 -tl 12 -ps -pss $j
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 5 -tl 12 -ps -pss $j
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 6 -tl 12 -ps -pss $j
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 7 -tl 12 -ps -pss $j
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 8 -tl 12 -ps -pss $j
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 9 -tl 12 -ps -pss $j
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 0 -tl 12 -ps -pss $k
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 1 -tl 12 -ps -pss $k
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 2 -tl 12 -ps -pss $k
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 3 -tl 12 -ps -pss $k
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 4 -tl 12 -ps -pss $k
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 5 -tl 12 -ps -pss $k
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 6 -tl 12 -ps -pss $k
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 7 -tl 12 -ps -pss $k
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 8 -tl 12 -ps -pss $k
+#python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 9 -tl 12 -ps -pss $k" >> s"$i".txt
+#done
+
+for i in {68..95..3}
 do
   echo "#!/bin/bash
 
@@ -8,7 +61,7 @@ do
 #SBATCH --time=12-00:00
 #SBATCH --job-name=sac_$i
 #SBATCH --output=%x-%j.out
-#SBATCH --mail-user=sschoepp@ualberta.ca
+#SBATCH --mail-user=taghianj@ualberta.ca
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
@@ -19,80 +72,76 @@ parallel < s$i.txt" >> s"$i".sh
 
   j=$((i+1))
   k=$((i+2))
-  echo "python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 0 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 1 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 2 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 3 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 4 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 5 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 6 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 7 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 8 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 9 -tl 12 -ps -pss $i
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 0 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 1 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 2 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 3 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 4 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 5 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 6 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 7 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 8 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 9 -tl 12 -ps -pss $j
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 0 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 1 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 2 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 3 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 4 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 5 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 6 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 7 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 8 -tl 12 -ps -pss $k
-python /home/sschoepp/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 9 -tl 12 -ps -pss $k" >> s"$i".txt
+  echo "python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 0 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 1 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 2 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 3 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 4 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 5 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 6 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 7 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 8 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 9 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 0 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 1 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 2 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 3 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 4 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 5 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 6 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 7 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 8 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 9 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 0 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 1 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 2 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 3 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 4 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 5 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 6 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 7 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 8 -tl 12 -ps -pss $k
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 9 -tl 12 -ps -pss $k" >> s"$i".txt
 done
 
-#for i in {50..98..2}
-#do
-#  echo "#!/bin/bash
-#
-##SBATCH --nodes=1
-##SBATCH --gres=gpu:v100:4
-##SBATCH --ntasks-per-node=20
-##SBATCH --mem=191000M
-##SBATCH --time=7-00:00
-##SBATCH --job-name=sac_$i
-##SBATCH --output=%x-%j.out
-##SBATCH --mail-user=taghianj@ualberta.ca
-##SBATCH --mail-type=BEGIN
-##SBATCH --mail-type=END
-##SBATCH --mail-type=FAIL
-##SBATCH --mail-type=REQUEUE
-##SBATCH --mail-type=ALL
-#
-#parallel < s$i.txt" >> s"$i".sh
-#done
-#
-#for i in {50..98..2}
-#do
-#  j=$((i+1))
-#  echo "python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 0 -tl 12 -ps -pss $i
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 1 -tl 12 -ps -pss $i
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 2 -tl 12 -ps -pss $i
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 3 -tl 12 -ps -pss $i
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 4 -tl 12 -ps -pss $i
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 5 -tl 12 -ps -pss $i
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 6 -tl 12 -ps -pss $i
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 7 -tl 12 -ps -pss $i
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 8 -tl 12 -ps -pss $i
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 9 -tl 12 -ps -pss $i
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 0 -tl 12 -ps -pss $j
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 1 -tl 12 -ps -pss $j
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 2 -tl 12 -ps -pss $j
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 3 -tl 12 -ps -pss $j
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 4 -tl 12 -ps -pss $j
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 5 -tl 12 -ps -pss $j
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 6 -tl 12 -ps -pss $j
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 7 -tl 12 -ps -pss $j
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 8 -tl 12 -ps -pss $j
-#python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 9 -tl 12 -ps -pss $j" >> s"$i".txt
-#done
+for i in 98
+do
+  echo "#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=20
+#SBATCH --mem=140G
+#SBATCH --time=12-00:00
+#SBATCH --job-name=sac_$i
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=taghianj@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < s$i.txt" >> s"$i".sh
+
+  j=$((i+1))
+  echo "python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 0 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 1 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 2 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 3 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 4 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 5 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 6 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 7 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 8 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 9 -tl 12 -ps -pss $i
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 0 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 1 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 2 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 3 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 4 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 5 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 6 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 7 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 8 -tl 12 -ps -pss $j
+python /home/taghianj/scratch/openai/controllers/sacv2/sacv2_n_controller.py -a --resumable -s 9 -tl 12 -ps -pss $j" >> s"$i".txt
+done

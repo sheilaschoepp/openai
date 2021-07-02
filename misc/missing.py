593e2cc55e1d036654b4edc4b35f8c66c9ec3ac9
Sheila Schoepp <sschoepp@ualberta.ca>
2021-07-02 15:07:17 -0600

    added missing code

--- misc/missing.py
+++ misc/missing.py
@@ -81,8 +81,8 @@ if __name__ == "__main__":
 
     RUNS = 10
 
-    # run("ppo", "fetchreach")
-    # run("sac", "fetchreach")
+    run("ppo", "fetchreach")
+    run("sac", "fetchreach")
     run("ppo", "ant")
     run("sac", "ant")
 

dec367d28132181e5c38508b38a3358b0f7d370f
Sheila Schoepp <sschoepp@ualberta.ca>
2021-07-03 19:05:09 -0600

    us

--- scripts/sheila/melco2.sh
+++ scripts/sheila/melco2.sh
@@ -1,4 +1,4 @@
-PPO_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/ppov2_n_controller.py"
+PPO_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/mod/ppov2_n_controller_mod.py"
 RESUME_FILE="/home/sschoepp/Documents/openai/data/PPOv2_Ant-v2:1000000000_lr:0.002463_lrd:True_g:0.9638_ns:3497_mbs:2048_epo:12_eps:0.1_c1:0.5_c2:0.006_cvl:False_mgn:0.5_gae:True_lam:0.9122_hd:64_lstd:0.0_tef:5000000_ee:10_tmsf:50000000_d:cpu_ps:True_pss:39"
 tmux new-session -d -s ppo390 "python $PPO_N_CONTROLLER_ABSOLUTE_PATH --resume --resume_file $RESUME_FILE/seed0"
 tmux new-session -d -s ppo391 "python $PPO_N_CONTROLLER_ABSOLUTE_PATH --resume --resume_file $RESUME_FILE/seed1"

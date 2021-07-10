58bceb3cec1f9758c8ad285e0b36efe60914a96b
Sheila Schoepp <sschoepp@ualberta.ca>
2021-07-10 10:41:08 -0600

    us

---
+++ scripts/graham/sac_v3a.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:p100:2
+#SBATCH --ntasks-per-node=10
+#SBATCH --mem=127518M
+#SBATCH --time=6-12:00
+#SBATCH --job-name=sac_v3a
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=sschoepp@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < sac_v3a.txt

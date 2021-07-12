e6f160315817a77b24545771a53cc6cbede7efaf
Sheila Schoepp <sschoepp@ualberta.ca>
2021-07-11 18:01:18 -0600

    us

--- scripts/setup/create_melco2_scripts.sh
+++ scripts/setup/create_melco2_scripts.sh
@@ -1,4 +1,5 @@
-echo 'RESUME_FILE="/local/melco2-1/shared/ant/normal/SACv2_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_a:True_d:cuda_ps:True_pss:61_resumed"' > melco2.sh
+echo 'SAC_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/sacv2_n_controller.py"' > melco2.sh
+echo 'RESUME_FILE="/local/melco2-1/shared/ant/normal/SACv2_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_a:True_d:cuda_ps:True_pss:61_resumed"' >> melco2.sh
 for s in {0..4}
 do
   echo 'tmux new-session -d -s sac61'$s' "CUDA_VISIBLE_DEVICES=0 python $SAC_N_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v3 --resume --resume_file $RESUME_FILE/seed'$s' -t 20000000 -tl 4"' >> melco2.sh
--- scripts/sheila/melco2.sh
+++ scripts/sheila/melco2.sh
@@ -1,3 +1,5 @@
+
+SAC_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/sacv2_n_controller.py"
 RESUME_FILE="/local/melco2/shared/SACv2_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_a:True_d:cuda_ps:True_pss:61_resumed"
 tmux new-session -d -s sac610 "CUDA_VISIBLE_DEVICES=0 python $SAC_N_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v3 --resume --resume_file $RESUME_FILE/seed0 -t 20000000 -tl 4"
 tmux new-session -d -s sac611 "CUDA_VISIBLE_DEVICES=0 python $SAC_N_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v3 --resume --resume_file $RESUME_FILE/seed1 -t 20000000 -tl 4"

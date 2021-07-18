3b7a3a5708c7e8804bfc5d27f394fa9cfe2924ec
Sheila Schoepp <sschoepp@ualberta.ca>
2021-07-17 18:44:47 -0600

    added ppo graham scripts

---
+++ scripts/graham/ppo/resume/ppo_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=10
+#SBATCH --mem=128000M
+#SBATCH --time=7-00:00
+#SBATCH --job-name=ppo_test
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=sschoepp@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < ppo_test.txt

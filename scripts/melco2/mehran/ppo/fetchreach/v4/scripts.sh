5c065534e2721e8b04ae9d72d85ecdc3bd0de8c5
mehran <taghianmehran@gmail.com>
2021-07-21 10:30:13 -0600

    fetch reach abnormal env v4 added for mehran

--- scripts/melco2/mehran/ppo/fetchreach/v4/scripts.sh
+++ scripts/melco2/mehran/ppo/fetchreach/v4/scripts.sh
@@ -1,4 +1,4 @@
-PPO_AB_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/ppov2_ab_controller.py"
+PPO_AB_CONTROLLER_ABSOLUTE_PATH="/home/taghianj/Documents/develop/openai/controllers/ppov2/ppov2_ab_controller.py"
 RESUME_FILE="/local/melco2-1/shared/fetchreach/normal/PPOv2_FetchReachEnv-v0:6000000_lr:0.000275_lrd:True_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:60000_d:cpu_ps:True_pss:43"
 tmux new-session -d -s ppov4-0 "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnv-v4 -t 6000000 -f $RESUME_FILE/seed0"
 tmux new-session -d -s ppov4-1 "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnv-v4 -t 6000000 -f $RESUME_FILE/seed1"

634f7e74cd7882afef9a96e5a2882fcd2df27676
Sheila Schoepp <sschoepp@ualberta.ca>
2021-07-21 14:41:50 -0600

    us

--- scripts/setup/create_resume_sac_scripts.sh
+++ scripts/setup/create_resume_sac_scripts.sh
@@ -1,12 +1,11 @@
 echo 'SAC_AB_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/sacv2_ab_controller.py"' > melco2.sh
-#echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/v1/SACv2_AntEnv-v1:20000000_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_crb:False_rn:False_a:True_d:cuda_r"' >> melco2.sh
-echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/v1/SACv2_AntEnv-v1:20000000_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_crb:False_rn:True_a:True_d:cuda_r"' >> melco2.sh
+#echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/v1/SACv2_AntEnv-v1:20000000_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_crb:False_rn:True_a:True_d:cuda_r"' >> melco2.sh
 #echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/v1/SACv2_AntEnv-v1:20000000_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_crb:True_rn:False_a:True_d:cuda_r"' >> melco2.sh
 #echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/v1/SACv2_AntEnv-v1:20000000_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_crb:True_rn:True_a:True_d:cuda_r"' >> melco2.sh
 #echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/v3/SACv2_AntEnv-v3:20000000_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_crb:False_rn:False_a:True_d:cuda_r"' >> melco2.sh
 #echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/v3/SACv2_AntEnv-v3:20000000_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_crb:False_rn:True_a:True_d:cuda_r"' >> melco2.sh
 #echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/v3/SACv2_AntEnv-v3:20000000_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_crb:True_rn:False_a:True_d:cuda_r"' >> melco2.sh
-#echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/v3/SACv2_AntEnv-v3:20000000_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_crb:True_rn:True_a:True_d:cuda_r"' >> melco2.sh
+echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/v3/SACv2_AntEnv-v3:20000000_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_crb:True_rn:True_a:True_d:cuda_r"' >> melco2.sh
 
 for i in {0..4}
 do

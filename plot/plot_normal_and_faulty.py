4267191ef8cf84584e2e9863686c3016c97e3678
mehran <taghianmehran@gmail.com>
2021-07-21 21:38:38 -0600

    plot file for faulty experiments added

---
+++ plot/plot_normal_and_faulty.py
@@ -0,0 +1,136 @@
+import seaborn as sns
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+import argparse
+import pathlib
+import os, shutil, pickle
+from tqdm import tqdm
+
+parser = argparse.ArgumentParser(description="Draw results of the experiments inside a directory")
+
+parser.add_argument("-nd", "--normaldir", default="",
+                    help="absolute path of the folder containing the seeds of the normal environment")
+
+parser.add_argument("-nf", "--faultydir", default="",
+                    help="absolute path of the folder containing the experiments (not seeds) of the faulty environment "
+                         "whose normal environment was the path provided in --normaldir")
+
+args = parser.parse_args()
+
+
+def find_pss(exp_dir):
+    """
+    Finds pss from the name of the file.
+    """
+    params = str.split(exp_dir, '_')
+    pss = 0
+    for p in reversed(params):
+        if 'pss' in p:
+            pss = str.split(p, ':')[-1]
+    return pss
+
+
+def find_env_name(exp_dir):
+    return exp_dir.split('_')[1].split(':')[0]
+
+
+X_AXIS = ['num_time_steps', 'num_updates', 'num_samples']
+
+
+def draw():
+    NORMAL_PATH = args.normaldir
+    FAULTY_PATH = args.faultydir
+    faulty_experiments_list = os.listdir(FAULTY_PATH)
+    env_name = find_env_name(faulty_experiments_list[0])
+
+    current_path = pathlib.Path(__file__).parent.absolute()
+    result_path = os.path.join(current_path, 'normal_faulty_plots', env_name)
+
+    if not os.path.exists(result_path):
+        os.mkdir(result_path)
+
+    experiment_seed = {}
+    experiments_statistical_info = {}
+    # set the theme for plots
+    sns.set_style("dark")
+    sns.set_theme()
+
+    normal_seed_list = os.listdir(NORMAL_PATH)
+    experiment_seed['normal'] = normal_seed_list
+
+    # List the experiments in a directory
+    for exp in faulty_experiments_list:
+        seed_list = os.listdir(os.path.join(FAULTY_PATH, exp))
+        experiment_seed[exp] = seed_list
+
+    # Calculate the average and standard error of each experiment and
+    # draw results for each one
+    with tqdm(total=len(faulty_experiments_list) + 1) as pbar:
+        for exp in experiment_seed.keys():
+            num_seeds = len(experiment_seed[exp])
+            average_returns = None
+
+            index = 0
+            for seed in experiment_seed[exp]:
+                path = os.path.join(FAULTY_PATH, exp, seed, 'csv', 'eval_data.csv')
+                try:
+                    data_temp = pd.read_csv(path)
+                except FileNotFoundError:
+                    continue
+                if average_returns is None:
+                    average_returns = np.zeros([num_seeds, len(data_temp) - 1])
+
+                average_returns[index] = np.array(data_temp['average_return'])[:-1]
+                index += 1
+
+            average = np.mean(average_returns, axis=0)
+            standard_error = np.std(average_returns, axis=0) / np.sqrt(average_returns.shape[0])
+
+            if exp == 'normal':
+                start_index = 0
+            else:
+                start_index = experiments_statistical_info['normal']['avg'].shape[0]
+
+            experiments_statistical_info[exp] = {'avg': average[start_index: -1],
+                                                 'std_error': standard_error[start_index: -1],
+                                                 'time_step': np.array(data_temp['num_time_steps'])[start_index:-1],
+                                                 'num_updates': np.array(data_temp['num_updates'])[start_index:-1],
+                                                 'num_samples': np.array(data_temp['num_samples'])[start_index:-1]}
+
+            # plt.figure(figsize=(12, 5))
+            # plt.plot(x, average, 'b')
+            # plt.fill_between(x, average - 2.26 * standard_error, average + 2.26 * standard_error, alpha=0.2)
+            # plt.savefig(os.path.join(result_path, f'{exp}.jpg'), dpi=300)
+            # plt.close()
+            # pbar.update(1)
+
+    # Finding the best HP settings based on the sum of average returns of different seeds
+    # sorted_experiments_score_sac = {k: v for k, v in sorted(experiments_score_sac.items(), key=lambda item: item[1])}
+    # sorted_experiments_score_ppo = {k: v for k, v in sorted(experiments_score_ppo.items(), key=lambda item: item[1])}
+
+    for x in X_AXIS:
+        plt.figure(figsize=(12, 5))
+
+        for exp in experiments_statistical_info:
+            x_values = experiments_statistical_info[exp][x]
+            average = experiments_statistical_info[exp]['avg']
+            plt.plot(x_values, average)
+            plt.fill_between(x, average - 2.26 * standard_error, average + 2.26 * standard_error, alpha=0.2)
+
+        plt.legend(loc="upper right")
+        plt.savefig(os.path.join(result_path, f'x_axis_{x}.jpg'), dpi=300)
+        plt.close()
+
+
+def single_plot(exp, experiments_statistical_info):
+    average = experiments_statistical_info[exp]['avg']
+    standard_error = experiments_statistical_info[exp]['std_error']
+    x = experiments_statistical_info[exp]['time_step']
+    label = experiments_statistical_info[exp]['pss']
+    plt.plot(x, average, label=label)
+    plt.fill_between(x, average - 2.26 * standard_error, average + 2.26 * standard_error, alpha=0.2)
+
+
+if __name__ == "__main__":
+    draw()

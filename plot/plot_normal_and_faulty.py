7ee74fbccf6903f88b21cfaa92526fa1d3579912
mehran <taghianmehran@gmail.com>
2021-07-22 18:02:32 -0600

    debugging plot_normal_and_faulty.py

--- plot/plot_normal_and_faulty.py
+++ plot/plot_normal_and_faulty.py
@@ -69,6 +69,7 @@ def draw():
     # set the theme for plots
     sns.set_style("dark")
     colors = sns.color_palette("colorblind", 10).as_hex()
+    print(colors)
     # sns.set_palette('colorblind')
     sns.set_theme()
 
@@ -151,7 +152,7 @@ def draw():
 
             # TODO: change ylim to be dynamic according to the results
             if exp != 'normal':
-                tmp = sub2.plot(x_values, average, colors=colors, label=label)[0]
+                tmp = sub2.plot(x_values, average, label=label)[0]
                 color = tmp.get_color()
                 sub1.plot(x_values, average, color=color)
                 sub1.fill_between(x_values, average - 2.26 * standard_error, average + 2.26 * standard_error, alpha=0.2)
@@ -159,7 +160,7 @@ def draw():
                 sub1.set_ylim(-5, 0)
                 sub1.set_ylabel('y', labelpad=15)
             else:
-                sub2.plot(x_values, average, colors=colors, label=label)
+                sub2.plot(x_values, average, label=label)
 
             if exp != 'normal' and not already_filled_interval:
                 already_filled_interval = True

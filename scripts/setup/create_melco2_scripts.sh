c076e245474e1889e8b585e0ba0299a3c0db0127
Sheila Schoepp <sschoepp@ualberta.ca>
2021-07-25 08:28:35 -0600

    us

--- scripts/setup/create_melco2_scripts.sh
+++ scripts/setup/create_melco2_scripts.sh
@@ -1,35 +1,28 @@
-echo 'SAC_AB_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/sacv2_ab_controller.py"' > melco2.sh
-echo 'RESUME_FILE="/local/melco2-1/shared/fetchreach/normal/SACv2_FetchReachEnvGE-v0:2000000_g:0.8097_t:0.0721_a:0.2_lr:0.001738_hd:256_rbs:10000_bs:512_mups:1_tui:1_tef:10000_ee:10_tmsf:20000_a:True_d:cuda_ps:True_pss:21"' >> melco2.sh
-
-for s in {0..4}
-do
-  echo 'tmux new-session -d -s sacv1GE-'$s' "CUDA_VISIBLE_DEVICES=0 python $SAC_AB_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnvGE-v1 -t 2000000 -c -f $RESUME_FILE/seed'$s'"' >> melco2.sh
-done
-for s in {5..9}
-do
-  echo 'tmux new-session -d -s sacv1GE-'$s' "CUDA_VISIBLE_DEVICES=1 python $SAC_AB_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnvGE-v1 -t 2000000 -c -f $RESUME_FILE/seed'$s'"' >> melco2.sh
-done
-
-
-echo 'PPO_AB_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/ppov2_ab_controller.py"' >> melco2.sh
-echo 'RESUME_FILE="/local/melco2-1/shared/fetchreach/normal/PPOv2_FetchReachEnvGE-v0:6000000_lr:0.000275_lrd:True_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:60000_d:cpu_ps:True_pss:43"' >> melco2.sh
+echo 'PPO_AB_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/mod2/ppov2_ab_controller.py"' > melco2.sh
+echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/ppo/PPOv2_AntEnv-v1:600000000_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:50000000_cm:False_rn:False_d:cpu_r"' >> melco2.sh
 
 for s in {0..9}
 do
-  echo 'tmux new-session -d -s ppov1GE-'$s' "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnvGE-v1 -t 6000000 -f $RESUME_FILE/seed'$s'"' >> melco2.sh
+  echo 'tmux new-session -d -s ppov1-'$s' "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH --resume -rf $RESUME_FILE/seed'$s'"' >> melco2.sh
 done
+
+echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/ppo/PPOv2_AntEnv-v1:600000000_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:50000000_cm:True_rn:False_d:cpu_r"' >> melco2.sh
 
 for s in {0..9}
 do
-  echo 'tmux new-session -d -s ppov1GEcm-'$s' "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnvGE-v1 -t 6000000 -f $RESUME_FILE/seed'$s' -cm"' >> melco2.sh
+  echo 'tmux new-session -d -s ppov1cm-'$s' "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH --resume -rf $RESUME_FILE/seed'$s'"' >> melco2.sh
 done
+
+echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/ppo/PPOv2_AntEnv-v1:600000000_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:50000000_cm:False_rn:True_d:cpu_r"' >> melco2.sh
 
 for s in {0..9}
 do
-  echo 'tmux new-session -d -s ppov1GErn-'$s' "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnvGE-v1 -t 6000000 -f $RESUME_FILE/seed'$s' -rn"' >> melco2.sh
+  echo 'tmux new-session -d -s ppov1rn-'$s' "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH --resume -rf $RESUME_FILE/seed'$s'"' >> melco2.sh
 done
+
+echo 'RESUME_FILE="/local/melco2-1/shared/ant/faulty/ppo/PPOv2_AntEnv-v1:600000000_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:50000000_cm:True_rn:True_d:cpu_r"' >> melco2.sh
 
 for s in {0..9}
 do
-  echo 'tmux new-session -d -s ppov1GEcmrn-'$s' "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnvGE-v1 -t 6000000 -f $RESUME_FILE/seed'$s' -cm -rn"' >> melco2.sh
+  echo 'tmux new-session -d -s ppov1cmrn-'$s' "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH --resume -rf $RESUME_FILE/seed'$s'"' >> melco2.sh
 done

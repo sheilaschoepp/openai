f5ed5c0edd347e4a21ea515aac2ffd912beffd01
Sheila Schoepp <sschoepp@ualberta.ca>
2021-07-26 22:09:20 -0600

    us

--- scripts/setup/create_melco1_scripts.sh
+++ scripts/setup/create_melco1_scripts.sh
@@ -1,14 +1,8 @@
 echo 'PPO_AB_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/mod2/ppov2_ab_controller.py"' > melco1.sh
-echo 'RESUME_FILE="/local/melco2/shared/PPOv2_AntEnv-v4:600000000_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:50000000_cm:True_rn:False_d:cpu_r"' >> melco1.sh
-
-for s in {7..9}
-do
-  echo 'tmux new-session -d -s ppov4cm-'$s' "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH --resume -rf $RESUME_FILE/seed'$s'"' >> melco1.sh
-done
 
 echo 'RESUME_FILE="/local/melco2/shared/PPOv2_AntEnv-v4:600000000_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:50000000_cm:False_rn:True_d:cpu_r"' >> melco1.sh
 
-for s in {0..3}
+for s in 4
 do
   echo 'tmux new-session -d -s ppov4rn-'$s' "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH --resume -rf $RESUME_FILE/seed'$s'"' >> melco1.sh
 done

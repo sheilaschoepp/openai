7a52a7fd2b1e9c269b8ece97b8bcaa7a6a175838
Sheila Schoepp <sschoepp@ualberta.ca>
2021-07-27 22:03:07 -0600

    udpated plots

--- plot/histogram/histogram.py
+++ plot/histogram/histogram.py
@@ -1004,7 +1004,7 @@ if __name__ == "__main__":
     collect_data = args.collect_data
 
     repeat_ant = False
-    repeat_fetchreach = True
+    repeat_fetchreach = False
 
     """ant normal"""
 
@@ -1142,10 +1142,12 @@ if __name__ == "__main__":
 
     """fetchreach normal"""
 
-    # PPO v0  # todo
+    if repeat_fetchreach:
 
-    # file = "/media/sschoepp/easystore/shared/fetchreach/normal/PPOv2_FetchReachEnv-v0:6000000_lr:0.000275_lrd:True_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:60000_d:cpu_ps:True_pss:43"
-    # draw_histogram()
+        # PPO v0
+
+        file = "/media/sschoepp/easystore/shared/fetchreach/normal/PPOv2_FetchReachEnv-v0:6000000_lr:0.000275_lrd:True_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:60000_d:cpu_ps:True_pss:43"
+        draw_histogram()
 
     # PPO v0GE  # todo
 

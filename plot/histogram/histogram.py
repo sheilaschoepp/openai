f1b49b6578372c6d923b141d16b76fa42225f7c4
Sheila Schoepp <sschoepp@ualberta.ca>
2021-07-27 21:04:49 -0600

    update to histogram

--- plot/histogram/histogram.py
+++ plot/histogram/histogram.py
@@ -1124,10 +1124,10 @@ if __name__ == "__main__":
 
     """fetchreach normal"""
 
-    # PPO v0
+    # PPO v0  # todo
 
-    file = "/media/sschoepp/easystore/shared/fetchreach/normal/PPOv2_FetchReachEnv-v0:6000000_lr:0.000275_lrd:True_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:60000_d:cpu_ps:True_pss:43"
-    draw_histogram()
+    # file = "/media/sschoepp/easystore/shared/fetchreach/normal/PPOv2_FetchReachEnv-v0:6000000_lr:0.000275_lrd:True_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:60000_d:cpu_ps:True_pss:43"
+    # draw_histogram()
 
     # PPO v0GE  # todo
 

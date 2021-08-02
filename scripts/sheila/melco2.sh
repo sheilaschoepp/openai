760d0a38733ab131a102cbd955762dfa84b1ed99
Sheila Schoepp <sschoepp@ualberta.ca>
2021-08-02 13:27:00 -0600

    us

--- scripts/sheila/melco2.sh
+++ scripts/sheila/melco2.sh
@@ -1,6 +1,11 @@
 SAC_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/mod/sacv2_n_controller.py"
 
-for s in {0..9}
+for s in {0..4}
 do
   tmux new-session -d -s sac-$i "CUDA_VISIBLE_DEVICES=0 python $SAC_N_CONTROLLER_ABSOLUTE_PATH -a -c --resumable -tmsf 200000 -s $s -ps -pss 61"
+done
+
+for s in {5..9}
+do
+  tmux new-session -d -s sac-$i "CUDA_VISIBLE_DEVICES=1 python $SAC_N_CONTROLLER_ABSOLUTE_PATH -a -c --resumable -tmsf 200000 -s $s -ps -pss 61"
 done

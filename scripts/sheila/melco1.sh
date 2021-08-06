eed2f8966595010b5ffcebe0600f045c9202b997
Sheila Schoepp <sschoepp@ualberta.ca>
2021-08-06 12:31:33 -0600

    us

--- scripts/sheila/melco1.sh
+++ scripts/sheila/melco1.sh
@@ -1,11 +1,11 @@
-SAC_AB_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/sacv2_ab_controller.py"
-RESUME_FILE="/home/sschoepp/Documents/openai/data/SACv2_AntEnv-v2:20000000_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:1000000_crb:True_rn:False_a:True_d:cuda_r"
+SAC_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/mod/sacv2_n_controller.py"
 
-for s in {0..4}
+for s in 14 15 16 17
 do
-  tmux new-session -d -s sac-$s "CUDA_VISIBLE_DEVICES=6 python $SAC_AB_CONTROLLER_ABSOLUTE_PATH -c --resume -rf $RESUME_FILE/seed$s"
+  tmux new-session -d -s sacv2-$s "CUDA_VISIBLE_DEVICES=6 python $SAC_N_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnv-v0 -a -c -t 2000000 -tef 10000 -tmsf 20000 -ps -pss 21"
 done
-for s in {5..9}
+
+for s in 18 19 20 21
 do
-  tmux new-session -d -s sac-$s "CUDA_VISIBLE_DEVICES=7 python $SAC_AB_CONTROLLER_ABSOLUTE_PATH -c --resume -rf $RESUME_FILE/seed$s"
+  tmux new-session -d -s sacv2-$s "CUDA_VISIBLE_DEVICES=7 python $SAC_N_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnv-v0 -a -c -t 2000000 -tef 10000 -tmsf 20000 -ps -pss 21"
 done

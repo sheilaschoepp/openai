962d0f323fc44894d642be23388b65218e48a55e
Sheila Schoepp <sschoepp@ualberta.ca>
2021-08-06 12:29:20 -0600

    us

--- scripts/sheila/melco2.sh
+++ scripts/sheila/melco2.sh
@@ -1,6 +1,18 @@
-PPO_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/ppov2_n_controller.py"
+#PPO_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/ppov2_n_controller.py"
+#
+#for s in {10..29}
+#do
+#  tmux new-session -d -s sac-$s "python $PPO_N_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnv-v0 -lrd -t 6000000 -tef 30000 -tmsf 60000  -ps -pss 43"
+#done
 
-for s in {10..29}
+SAC_N_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/mod/sacv2_n_controller.py"
+
+for s in 10 11
 do
-  tmux new-session -d -s sac-$s "python $PPO_N_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnv-v0 -lrd -t 6000000 -tef 30000 -tmsf 60000  -ps -pss 43"
+  tmux new-session -d -s sacv2-$s "CUDA_VISIBLE_DEVICES=0 python $SAC_N_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnv-v0 -a -c -t 2000000 -tef 10000 -tmsf 20000 -ps -pss 21"
+done
+
+for s in 12 13
+do
+  tmux new-session -d -s sacv2-$s "CUDA_VISIBLE_DEVICES=1 python $SAC_N_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnv-v0 -a -c -t 2000000 -tef 10000 -tmsf 20000 -ps -pss 21"
 done

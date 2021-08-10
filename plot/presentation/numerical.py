db358e907c594c85fb868bd0be71218e5b2315d9
Sheila Schoepp <sschoepp@ualberta.ca>
2021-08-09 22:03:38 -0600

    numerical

--- plot/presentation/numerical.py
+++ plot/presentation/numerical.py
@@ -8,10 +8,15 @@ def check_recovery(directory):
 
     for dir_ in os.listdir(directory):
 
+        if dir_[-2:] == "_r":
+            continue
+
         # dir_ has format: SACv2_FetchReachEnvGE-v1:2000000_FetchReachEnvGE-v0:2000000_g:0.8097_t:0.0721_a:0.2_lr:0.001738_hd:256_rbs:10000_bs:512_mups:1_tui:1_tef:10000_ee:10_tmsf:20000_crb:False_rn:False_a:True_d:cuda
         parameters = dir_.split("_")
 
         # obtain setting
+        algorithm = parameters[0]
+        env_name = parameters[1].split(":")[0]
         mem = None
         rn = None
         for p in parameters:
@@ -19,6 +24,8 @@ def check_recovery(directory):
                 mem = eval(p.split(":")[1])
             elif "rn:" in p:
                 rn = eval(p.split(":")[1])
+
+        print(env_name, algorithm, "mem={}".format(mem), "rn:{}".format(rn))
 
         # obtain data for setting: mean and standard error
 
@@ -41,13 +48,14 @@ def check_recovery(directory):
 
         # check difference between pre-fault and post-recovery performance
 
+        interval_size = 10
+
         prefault = df_mean[0:201]
-        prefault_interval = prefault.iloc[-10:, 1:]
+        prefault_interval = prefault.iloc[-interval_size:, 1:]
 
         postfault = df_mean[201:]
         start = 0
         end = postfault.count()[1]
-        interval_size = 10
 
         finished = False
         while not finished:
@@ -62,26 +70,49 @@ def check_recovery(directory):
             # https://en.wikipedia.org/wiki/Welch%27s_t-test
             # https://en.wikipedia.org/wiki/Student%27s_t-distribution
 
-            numerator = postfault_interval.mean() - prefault_interval.mean()
-            denominator = np.sqrt((postfault_interval.std()/np.sqrt(interval_size))**2 + (prefault_interval.std()/np.sqrt(interval_size))**2)
+            # null: postfault_interval == prefault_interval
+            # alternative: postfault_interval != prefault_interval
 
-            t = numerator / denominator
+            # significance level
+            alpha = 0.05
 
-            t, p = stats.ttest_ind(postfault_interval.reset_index().values, prefault_interval.values, equal_var=False)
-            
+            t, p = stats.ttest_ind(postfault_interval.values, prefault_interval.values, equal_var=False)
+
+            # Wilcoxon Signed Rank Test
+            # https://en.wikipedia.org/wiki/Wilcoxon_signed-rank_test
+            # https://sphweb.bumc.bu.edu/otlt/mph-modules/bs/bs704_nonparametric/BS704_Nonparametric6.html
+
+            x = postfault_interval.reset_index().iloc[:, 1:].values.squeeze()
+            y = prefault_interval.reset_index().iloc[:, 1:].values.squeeze()
+            w, p2 = stats.wilcoxon(x, y)
+
+            if w <= 8:
+                # reject null hypothesis
+                pass
+            else:
+                # accept null hypothesis
+                pass
+
+            if p <= alpha:
+                # reject null hypothesis; evidence that performance has changed
+                pass
+            else:
+                # accept null hypothesis; performance has not changed
+                print(start, start + interval_size)
+                break
+
             start += 1
-            
 
 
 if __name__ == "__main__":
 
     # local for Ant PPO
-    ppo_data_dir = "/media/sschoepp/easystore/shared/ant/faulty/ppo"
-    # ppo_data_dir = "/mnt/DATA/shared/ant/faulty/ppo"
+    # ppo_data_dir = "/media/sschoepp/easystore/shared/ant/faulty/ppo"
+    ppo_data_dir = "/mnt/DATA/shared/fetchreach/faulty/ppo"
 
     # v1
 
-    directory = os.path.join(ppo_data_dir, "v1")
+    directory = os.path.join(ppo_data_dir, "v4")
     check_recovery(directory)
 
     print(1)

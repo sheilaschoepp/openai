abbf1986c5bb54ede9d9a321f442460894a08c72
Sheila Schoepp <sschoepp@ualberta.ca>
2021-08-09 20:55:46 -0600

    numerical

--- plot/presentation/numerical.py
+++ plot/presentation/numerical.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 
 def check_recovery(directory):
@@ -37,23 +38,46 @@ def check_recovery(directory):
         df = df.groupby(df.index)
 
         df_mean = df.mean()
-        df_var = df.var()
 
         # check difference between pre-fault and post-recovery performance
 
-        prefault = df[0:201]
-        prefault = prefault.iloc[-1:, 1:]
+        prefault = df_mean[0:201]
+        prefault_interval = prefault.iloc[-10:, 1:]
 
-        postfault = df[201:]
+        postfault = df_mean[201:]
+        start = 0
+        end = postfault.count()[1]
+        interval_size = 10
 
-        print(1)
+        finished = False
+        while not finished:
+            if start + interval_size > end:
+                finished = True
+                postfault_interval = postfault.iloc[-interval_size:, 1:]
+            else:
+                postfault_interval = postfault.iloc[start:start+interval_size, 1:]
+
+            # welch's t-test
+            # assume unequal variances
+            # https://en.wikipedia.org/wiki/Welch%27s_t-test
+            # https://en.wikipedia.org/wiki/Student%27s_t-distribution
+
+            numerator = postfault_interval.mean() - prefault_interval.mean()
+            denominator = np.sqrt((postfault_interval.std()/np.sqrt(interval_size))**2 + (prefault_interval.std()/np.sqrt(interval_size))**2)
+
+            t = numerator / denominator
+
+            t, p = stats.ttest_ind(postfault_interval.reset_index().values, prefault_interval.values, equal_var=False)
+            
+            start += 1
+            
 
 
 if __name__ == "__main__":
 
     # local for Ant PPO
-    # ppo_data_dir = "/media/sschoepp/easystore/shared/ant/faulty/ppo"
-    ppo_data_dir = "/mnt/DATA/shared/ant/faulty/ppo"
+    ppo_data_dir = "/media/sschoepp/easystore/shared/ant/faulty/ppo"
+    # ppo_data_dir = "/mnt/DATA/shared/ant/faulty/ppo"
 
     # v1
 

4b0ea5286b5b43add0b8df85f72776997ee8b088
Sheila Schoepp <sschoepp@ualberta.ca>
2021-08-17 11:38:04 -0600

    added ppo scripts for cedar/beluga

--- scripts/setup/create_ppo_scripts.sh
+++ scripts/setup/create_ppo_scripts.sh
@@ -3,8 +3,8 @@ echo "#!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=30
 #SBATCH --mem=191000M
-#SBATCH --time=1-00:00
-#SBATCH --job-name=ppo_v1a
+#SBATCH --time=7-00:00
+#SBATCH --job-name=ppo_v1b
 #SBATCH --output=%x-%j.out
 #SBATCH --mail-user=sschoepp@ualberta.ca
 #SBATCH --mail-type=BEGIN
@@ -13,19 +13,143 @@ echo "#!/bin/bash
 #SBATCH --mail-type=REQUEUE
 #SBATCH --mail-type=ALL
 
-parallel < ppo_v1a.txt" > ppo_v1a.sh
+parallel < ppo_v1b.txt" > ppo_v1b.sh
 
-for s in {20..29}
+echo "" > ppo_v1b.txt
+
+for s in {17..23}
 do
-  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_ab_controller.py -e FetchReachEnv-v1 -t 6000000 -f /home/sschoepp/scratch/openai/data/PPOv2_FetchReachEnv-v0:6000000_lr:0.000275_lrd:True_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:60000_d:cpu_ps:True_pss:43/seed$s" >> ppo_v1a.txt
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v1 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable" >> ppo_v1b.txt
 done
 
-for s in {20..29}
+for s in {17..23}
 do
-  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_ab_controller.py -e FetchReachEnv-v1 -t 6000000 -f /home/sschoepp/scratch/openai/data/PPOv2_FetchReachEnv-v0:6000000_lr:0.000275_lrd:True_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:60000_d:cpu_ps:True_pss:43/seed$s -cm" >> ppo_v1a.txt
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v1 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable -cm" >> ppo_v1b.txt
 done
 
-for s in {20..29}
+for s in {17..23}
 do
-  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/ppov2_ab_controller.py -e FetchReachEnv-v1 -t 6000000 -f /home/sschoepp/scratch/openai/data/PPOv2_FetchReachEnv-v0:6000000_lr:0.000275_lrd:True_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:60000_d:cpu_ps:True_pss:43/seed$s -rn" >> ppo_v1a.txt
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v1 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable -rn" >> ppo_v1b.txt
+done
+
+for s in {17..23}
+do
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v1 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable -cm -rn" >> ppo_v1b.txt
+done
+
+echo "#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=30
+#SBATCH --mem=191000M
+#SBATCH --time=7-00:00
+#SBATCH --job-name=ppo_v2b
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=sschoepp@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < ppo_v2b.txt" > ppo_v2b.sh
+
+echo "" > ppo_v2b.txt
+
+for s in {17..23}
+do
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v2 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable" >> ppo_v2b.txt
+done
+
+for s in {17..23}
+do
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v2 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable -cm" >> ppo_v2b.txt
+done
+
+for s in {17..23}
+do
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v2 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable -rn" >> ppo_v2b.txt
+done
+
+for s in {17..23}
+do
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v2 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable -cm -rn" >> ppo_v2b.txt
+done
+
+echo "#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=30
+#SBATCH --mem=191000M
+#SBATCH --time=7-00:00
+#SBATCH --job-name=ppo_v3b
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=sschoepp@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < ppo_v3b.txt" > ppo_v3b.sh
+
+echo "" > ppo_v3b.txt
+
+for s in {17..23}
+do
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v3 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable" >> ppo_v3b.txt
+done
+
+for s in {17..23}
+do
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v3 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable -cm" >> ppo_v3b.txt
+done
+
+for s in {17..23}
+do
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v3 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable -rn" >> ppo_v3b.txt
+done
+
+for s in {17..23}
+do
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v3 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable -cm -rn" >> ppo_v3b.txt
+done
+
+echo "#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=30
+#SBATCH --mem=191000M
+#SBATCH --time=7-00:00
+#SBATCH --job-name=ppo_v4b
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=sschoepp@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < ppo_v4b.txt" > ppo_v4b.sh
+
+echo "" > ppo_v4b.txt
+
+for s in {17..23}
+do
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v3 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable" >> ppo_v4b.txt
+done
+
+for s in {17..23}
+do
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v3 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable -cm" >> ppo_v4b.txt
+done
+
+for s in {17..23}
+do
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v3 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable -rn" >> ppo_v4b.txt
+done
+
+for s in {17..23}
+do
+  echo "python /home/sschoepp/scratch/openai/controllers/ppov2/mod/ppov2_ab_controller.py -e AntEnv-v3 -t 600000000 -f /home/sschoepp/scratch/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33/seed$s -tl 7 --resumable -cm -rn" >> ppo_v4b.txt
 done

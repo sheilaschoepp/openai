04d5e6a56fc9b14878ae55574ef62856ffda5afd
mehran <taghianmehran@gmail.com>
2021-08-20 16:13:45 -0600

    sac resume with mod added for seeds 10-16

---
+++ scripts/beluga/antsac_mehran/runall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sbatch sac_test.sh
+sbatch sac_v34e.sh
+sbatch sac_v12e.sh
+sbatch sac_v4d.sh
+sbatch sac_v3d.sh
+sbatch sac_v2d.sh
+sbatch sac_v1d.sh
---
+++ scripts/cedar/antsac_mehran/sac_v3d.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:v100l:4
+#SBATCH --ntasks-per-node=30
+#SBATCH --mem=191000M
+#SBATCH --time=7-00:00
+#SBATCH --job-name=sac_v3d
+#SBATCH --output=%x-%j.out
+#SBATCH --mail-user=taghianj@ualberta.ca
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+#SBATCH --mail-type=ALL
+
+parallel < sac_v3d.txt

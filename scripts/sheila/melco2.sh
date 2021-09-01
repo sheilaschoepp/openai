58181d23cd30aced71b3586d884183612eaaf9a0
Sheila Schoepp <sschoepp@ualberta.ca>
2021-08-31 19:20:30 -0600

    us

--- scripts/sheila/melco2.sh
+++ scripts/sheila/melco2.sh
@@ -24,14 +24,14 @@
 PPO_AB_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/mod/ppov2_ab_controller.py"
 FILE="/local/melco2-1/shared/ant/seeds/faulty/ppo/v4/old/PPOv2_AntEnv-v4:600000000_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_cm:True_rn:False_d:cpu_r"
 
-for s in {10..29}
+for s in 11 13 14 15 17 18 19 20 22
 do
-  tmux new-session -d -s ppo-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH --resume -rf $FILE/seed$s"
+  tmux new-session -d -s ppo3-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH --resume -rf $FILE/seed$s"
 done
 
 FILE="/local/melco2-1/shared/ant/seeds/faulty/ppo/v4/old/PPOv2_AntEnv-v4:600000000_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_cm:True_rn:True_d:cpu_r"
 
-for s in {10..29}
+for s in 10 11 14 16 17 18 20 22
 do
-  tmux new-session -d -s ppo2-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH --resume -rf $FILE/seed$s"
+  tmux new-session -d -s ppo4-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH --resume -rf $FILE/seed$s"
 done

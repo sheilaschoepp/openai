f50e99c8e6afc52ffe91ac2cadc62d7a576700fc
Sheila Schoepp <sschoepp@ualberta.ca>
2021-09-02 15:02:28 -0600

    us

--- scripts/sheila/new.sh
+++ scripts/sheila/new.sh
@@ -1,3 +1,3 @@
 SAC_AB_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/sacv2/mod/sacv2_ab_controller.py"
 RESUME_FILE="/media/sschoepp/easystore/shared/ant/seeds/normal/new/SACv2_Ant-v2:20000000_g:0.9646_t:0.0877_a:0.2_lr:0.001092_hd:256_rbs:500000_bs:512_mups:1_tui:1_tef:100000_ee:10_tmsf:100000_a:True_d:cuda_ps:True_pss:61_resumed_mod"
-tmux new-session -d -s sacv2-14 "CUDA_VISIBLE_DEVICES=1 python $SAC_AB_CONTROLLER_ABSOLUTE_PATH -c -e AntEnv-v2 -f $RESUME_FILE/seed14 -t 20000000"
+tmux new-session -d -s sacv1-17 "python $SAC_AB_CONTROLLER_ABSOLUTE_PATH -c -e AntEnv-v1 -f $RESUME_FILE/seed17 -t 20000000"

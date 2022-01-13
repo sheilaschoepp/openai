7995e8c298c847494f853aa24f4d88db3ba99707
Sheila Schoepp <sschoepp@ualberta.ca>
2022-01-12 20:42:50 -0700

    us

--- scripts/sheila/melco2.sh
+++ scripts/sheila/melco2.sh
@@ -1,5 +1,5 @@
 PPO_AB_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/mod2/ppov2_ab_controller.py"
-FILE="/home/sschoepp/Documents/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:60000000_ee:10_tmsf:50000000_d:cpu_ps:True_pss:33_resumed"
+FILE="/home/sschoepp/Documents/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:50000000_d:cpu_ps:True_pss:33_resumed"
 
 for s in {0..9}
 do
@@ -8,7 +8,7 @@ do
 done
 
 
-FILE="/home/sschoepp/Documents/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:60000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33"
+FILE="/home/sschoepp/Documents/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33"
 
 for s in {10..29}
 do

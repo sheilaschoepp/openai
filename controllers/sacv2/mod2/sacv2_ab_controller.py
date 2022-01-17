600408969eb3866f92d89b58b7bddcb872254d63
Sheila Schoepp <sschoepp@ualberta.ca>
2022-01-17 12:06:13 -0700

    updated controller

--- controllers/sacv2/mod2/sacv2_ab_controller.py
+++ controllers/sacv2/mod2/sacv2_ab_controller.py
@@ -459,7 +459,7 @@ class AbnormalController:
 
             real_time = int(time.time() - self.start)
 
-            index = int(num_time_steps / self.parameters["time_step_eval_frequency"]) + 1  # add 1 because we evaluate policy before learning
+            index = int((num_time_steps - 20000000) / self.parameters["time_step_eval_frequency"]) + 1 + 200  # add 1 because we evaluate policy before learning
             self.eval_data[index] = [num_time_steps, num_updates, num_samples, average_return, real_time]
 
             print("evaluation at {} time steps: {}".format(num_time_steps, average_return))

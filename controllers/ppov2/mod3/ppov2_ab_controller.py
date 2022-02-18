d5c67d18a49d1f918593f76cde952ddafa5148db
Sheila Schoepp <sschoepp@ualberta.ca>
2022-02-18 00:51:04 -0700

    updates

--- controllers/ppov2/mod3/ppov2_ab_controller.py
+++ controllers/ppov2/mod3/ppov2_ab_controller.py
@@ -475,8 +475,8 @@ class AbnormalController:
 
             real_time = int(time.time() - self.start)
 
-            index = (num_time_steps // self.parameters["time_step_eval_frequency"]) + 1  # add 1 because we evaluate policy before learning
-            self.eval_data[index] = [num_time_steps, num_updates, num_epoch_updates, num_mini_batch_updates, num_samples, average_return, real_time]
+            index = int((num_time_steps - 6000000) / self.parameters["time_step_eval_frequency"]) + 1 + 200  # add 1 because we evaluate policy before learning
+            self.eval_data[index] = [num_time_steps, num_updates, num_samples, average_return, real_time]
 
             print("evaluation at {} time steps: {}".format(num_time_steps, average_return))
 

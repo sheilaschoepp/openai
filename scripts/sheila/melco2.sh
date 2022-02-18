2a9dead576a2ac8408ed92635dd11d7785b521de
Sheila Schoepp <sschoepp@ualberta.ca>
2022-02-18 00:35:49 -0700

    updates

--- scripts/sheila/melco2.sh
+++ scripts/sheila/melco2.sh
@@ -1,17 +1,14 @@
 PPO_AB_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppov2/mod2/ppov2_ab_controller.py"
-FILE="/home/sschoepp/Documents/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:50000000_d:cpu_ps:True_pss:33_resumed"
+FILE="/local/melco2-1/shared/fetchreach/seeds/normal/PPOv2_FetchReachEnv-v0:6000000_lr:0.000275_lrd:True_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:60000_d:cpu_ps:True_pss:43"
 
-for s in {0..9}
+for s in {0..5}
 do
-  tmux new-session -d -s ppov1-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v1 -t 60000000 -f $FILE/seed$s; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v1 -t 60000000 -f $FILE/seed$s -cm; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v1 -t 60000000 -f $FILE/seed$s -rn; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v1 -t 60000000 -f $FILE/seed$s -cm -rn; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v3 -t 60000000 -f $FILE/seed$s; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v3 -t 60000000 -f $FILE/seed$s -cm; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v3 -t 60000000 -f $FILE/seed$s -rn; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v3 -t 60000000 -f $FILE/seed$s -cm -rn"
-  tmux new-session -d -s ppov2-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v2 -t 60000000 -f $FILE/seed$s; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v2 -t 60000000 -f $FILE/seed$s -cm; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v2 -t 60000000 -f $FILE/seed$s -rn; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v2 -t 60000000 -f $FILE/seed$s -cm -rn; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v4 -t 60000000 -f $FILE/seed$s; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v4 -t 60000000 -f $FILE/seed$s -cm; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v4 -t 60000000 -f $FILE/seed$s -rn; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v4 -t 60000000 -f $FILE/seed$s -cm -rn"
-done
-
-
-FILE="/home/sschoepp/Documents/openai/data/PPOv2_Ant-v2:600000000_lr:0.000123_lrd:True_slrd:0.25_g:0.9839_ns:2471_mbs:1024_epo:5_eps:0.3_c1:1.0_c2:0.0019_cvl:False_mgn:0.5_gae:True_lam:0.911_hd:64_lstd:0.0_tef:3000000_ee:10_tmsf:6000000_d:cpu_ps:True_pss:33"
-
-for s in {10..29}
-do
-  tmux new-session -d -s ppov3-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v1 -t 60000000 -f $FILE/seed$s; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v1 -t 60000000 -f $FILE/seed$s -cm; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v1 -t 60000000 -f $FILE/seed$s -rn; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v1 -t 60000000 -f $FILE/seed$s -cm -rn; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v3 -t 60000000 -f $FILE/seed$s; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v3 -t 60000000 -f $FILE/seed$s -cm; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v3 -t 60000000 -f $FILE/seed$s -rn; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v3 -t 60000000 -f $FILE/seed$s -cm -rn"
-  tmux new-session -d -s ppov4-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v2 -t 60000000 -f $FILE/seed$s; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v2 -t 60000000 -f $FILE/seed$s -cm; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v2 -t 60000000 -f $FILE/seed$s -rn; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v2 -t 60000000 -f $FILE/seed$s -cm -rn; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v4 -t 60000000 -f $FILE/seed$s; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v4 -t 60000000 -f $FILE/seed$s -cm; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v4 -t 60000000 -f $FILE/seed$s -rn; python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v4 -t 60000000 -f $FILE/seed$s -cm -rn"
+  tmux new-session -d -s ppov1cmrn-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v1 -t 100000 -f $FILE/seed$s -cm -rn"
+  tmux new-session -d -s ppov1-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v1 -t 100000 -f $FILE/seed$s"
+  tmux new-session -d -s ppov2cmrn-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v2 -t 100000 -f $FILE/seed$s -cm -rn"
+  tmux new-session -d -s ppov2-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v2 -t 100000 -f $FILE/seed$s"
+  tmux new-session -d -s ppov3cmrn-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v3 -t 100000 -f $FILE/seed$s -cm -rn"
+  tmux new-session -d -s ppov3-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v3 -t 100000 -f $FILE/seed$s"
+  tmux new-session -d -s ppov4cmrn-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v4 -t 100000 -f $FILE/seed$s -cm -rn"
+  tmux new-session -d -s ppov4-$s "python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e AntEnv-v4 -t 100000 -f $FILE/seed$s"
 done

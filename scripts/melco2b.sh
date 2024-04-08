5786e534033cb1ddec3e6cf532ad029db0c6c86a
Sheila Schoepp <sschoepp@ualberta.ca>
2024-04-08 14:38:13 -0600

    us

--- scripts/melco2b.sh
+++ scripts/melco2b.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
-# Paths and environment setup
-PPO_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppo/ppo_n_controller.py"
+# Constants
+PPO_AB_CONTROLLER_ABSOLUTE_PATH="/home/sschoepp/Documents/openai/controllers/ppo/ppo_ab_controller.py"
+FOLDER_PATH="/home/sschoepp/Documents/openai/data/PPOv2_FetchReachEnv-v0:6000000_lr:0.000275_lrd:True_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:60000_d:cpu_ps:True_pss:43"
+ENVIRONMENTS=("FetchReachEnv-v4" "FetchReachEnv-v6")
+COMMANDS=("-cm -rn" "-cm" "-rn" "")
 
-# Total number of tmux processes to be launched, each with a unique seed from 0 to 29
-total_tmux=30
+# Function to launch tmux sessions
+launch_session() {
+    local env=$1
+    local seed=$2
+    local cmd_option=$3
+    local session_name="env_${env}_seed_${seed}_cmd_${cmd_option// /_}"
+    local folder="${FOLDER_PATH}/seed${seed}"
+    local full_command="python $PPO_AB_CONTROLLER_ABSOLUTE_PATH -e $env -t 6000000 $cmd_option -f $folder -d"
 
-# PPOv2_FetchReachEnv-v0:6000000_lr:0.000275_lrd:True_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:60000_d:cpu_ps:True_pss:43
-#
+    tmux new-session -d -s "$session_name" "$full_command"
+    echo "Launched tmux session: $session_name"
+}
 
-# Launching tmux sessions
-for ((seed=0; seed<total_tmux; seed++)); do
-    session_name="session_seed${seed}"
-    tmux new-session -d -s "$session_name" "python $PPO_CONTROLLER_ABSOLUTE_PATH -e FetchReachEnv-v0 -t 6000000 -lrd -tef 30000 -tmsf 30000 -ps -pss 43 -s $seed -d"
-    echo "Session $session_name started with seed $seed."
+# Main loop to create tmux sessions
+for env in "${ENVIRONMENTS[@]}"; do
+    for seed in {0..29}; do
+        for cmd in "${COMMANDS[@]}"; do
+            # Check active tmux sessions and wait if they reach the limit
+            while [ $(tmux ls | wc -l) -ge 50 ]; do
+                echo "Maximum number of tmux sessions reached. Waiting..."
+                sleep 10  # Wait for 10 seconds before checking again
+            done
+            launch_session "$env" "$seed" "$cmd"
+        done
+    done
 done
 
-echo "All tmux processes launched."
+echo "All sessions are scheduled."

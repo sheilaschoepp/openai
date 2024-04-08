48d91e4164e38d7d8bef07778d347db5f25cd05c
Sheila Schoepp <sschoepp@ualberta.ca>
2024-04-08 12:06:08 -0600

    us

--- scripts/sheila/melco2b.sh
+++ scripts/sheila/melco2b.sh
@@ -8,6 +8,7 @@ total_tmux=30
 
 # PPOv2_FetchReachEnv-v0:6000000_lr:0.000275_lrd:True_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:60000_d:cpu_ps:True_pss:43
 # PPOv2_FetchReachEnv-v0:6000000_lr:0.000275_lrd:False_slrd:0.25_g:0.848_ns:3424_mbs:8_epo:24_eps:0.3_c1:1.0_c2:0.0007_cvl:False_mgn:0.5_gae:True_lam:0.9327_hd:64_lstd:0.0_tef:30000_ee:10_tmsf:30000_d:cpu_ps:True_pss:43
+
 # Launching tmux sessions
 for ((seed=0; seed<total_tmux; seed++)); do
     session_name="session_seed${seed}"

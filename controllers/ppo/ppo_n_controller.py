d9586eab07424c7bac09d0290965a3cd911ea691
Sheila Schoepp <sschoepp@ualberta.ca>
2024-11-11 23:46:08 -0700

    commented out code that checks if a directory exists; changed n_jobs to 1

--- controllers/ppo/ppo_n_controller.py
+++ controllers/ppo/ppo_n_controller.py
@@ -180,39 +180,40 @@ class NormalController:
         self.data_dir = os.getenv("HOME") + "/Documents/openai/data/" + self.experiment + "/seed" + str(
             self.parameters["seed"])
 
-        # are we restarting training?  do the data files for the
-        # selected seed already exist?
-        if path.exists(self.data_dir):
-
-            print(self.LINE)
-            print(self.LINE)
-
-            if args.delete:
-                # yes; argument flag present to indicate data deletion
-                print(colored("argument indicates DATA DELETION", "red"))
-                print(colored("deleting data...", "red"))
-                rmtree(self.data_dir, ignore_errors=True)
-                print(colored("data deletion complete", "red"))
-            else:
-                # yes; argument flag not present; get confirmation of data deletion from user input
-                print(colored(
-                    "You are about to delete saved data and restart training.",
-                    "red"))
-                s = input(colored(
-                    "Are you sure you want to continue?  Hit 'y' then 'Enter' to continue.\n",
-                    "red"))
-                if s == "y":
-                    # delete old data; rewrite new data to same location
-                    print(colored("user input indicates DATA DELETION", "red"))
-                    print(colored("deleting data...", "red"))
-                    rmtree(self.data_dir, ignore_errors=True)
-                    print(colored("data deletion complete", "red"))
-                else:
-                    # do not delete old data; system exit
-                    print(
-                        colored("user input indicates NO DATA DELETION", "red"))
-                    print(self.LINE)
-                    sys.exit("\nexiting...")
+        # TODO: uncomment this block of code
+        # # are we restarting training?  do the data files for the
+        # # selected seed already exist?
+        # if path.exists(self.data_dir):
+        #
+        #     print(self.LINE)
+        #     print(self.LINE)
+        #
+        #     if args.delete:
+        #         # yes; argument flag present to indicate data deletion
+        #         print(colored("argument indicates DATA DELETION", "red"))
+        #         print(colored("deleting data...", "red"))
+        #         rmtree(self.data_dir, ignore_errors=True)
+        #         print(colored("data deletion complete", "red"))
+        #     else:
+        #         # yes; argument flag not present; get confirmation of data deletion from user input
+        #         print(colored(
+        #             "You are about to delete saved data and restart training.",
+        #             "red"))
+        #         s = input(colored(
+        #             "Are you sure you want to continue?  Hit 'y' then 'Enter' to continue.\n",
+        #             "red"))
+        #         if s == "y":
+        #             # delete old data; rewrite new data to same location
+        #             print(colored("user input indicates DATA DELETION", "red"))
+        #             print(colored("deleting data...", "red"))
+        #             rmtree(self.data_dir, ignore_errors=True)
+        #             print(colored("data deletion complete", "red"))
+        #         else:
+        #             # do not delete old data; system exit
+        #             print(
+        #                 colored("user input indicates NO DATA DELETION", "red"))
+        #             print(self.LINE)
+        #             sys.exit("\nexiting...")
 
         # data
 
@@ -911,7 +912,7 @@ def main():
                                 direction="maximize",
                                 load_if_exists=True)
 
-    study.optimize(objective, n_trials=20, n_jobs=20)
+    study.optimize(objective, n_trials=20, n_jobs=1)
 
     print("Best hyperparameters found:")
     print(study.best_params)

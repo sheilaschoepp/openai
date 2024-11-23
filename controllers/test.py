ec39cf1d117f442ebf0258fe280f9975223fc94c
Sheila Schoepp <sschoepp@ualberta.ca>
2024-11-23 16:56:15 -0700

    u

--- controllers/test.py
+++ controllers/test.py
@@ -4,17 +4,14 @@ import optuna
 # Fetch the database URL from the environment variable
 database_url = os.environ.get("SAC_OPTUNA_DB_URL")
 
-# Create an Optuna study
-study = optuna.create_study(
-    study_name="example_study",
-    storage=database_url,
-    load_if_exists=True,
-    direction="minimize",
-)
+# Check if the URL is loaded
+if not database_url:
+    raise ValueError("Database URL not found in environment. Make sure OPTUNA_DB_URL is set.")
 
-# Add a dummy trial
-def objective(trial):
-    x = trial.suggest_float("x", -10, 10)
-    return (x - 2) ** 2
+# Name of the study to delete
+study_name = "example_study"
 
-study.optimize(objective, n_trials=1)
+# Delete the study
+optuna.delete_study(study_name=study_name, storage=database_url)
+
+print(f"Study '{study_name}' has been successfully deleted.")

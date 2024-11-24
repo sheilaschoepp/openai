eb1e8b87e6f29e5524eb77e66e0f9ae41ffce41a
Sheila Schoepp <sschoepp@ualberta.ca>
2024-11-24 15:57:04 -0700

    added wandb and logging of evaluation data

--- controllers/sac/sac_n_controller.py
+++ controllers/sac/sac_n_controller.py
@@ -17,6 +17,7 @@ import random
 import sys
 import time
 import torch
+import wandb
 
 from copy import copy, deepcopy
 from datetime import date, timedelta
@@ -121,6 +122,13 @@ class NormalController:
                            "device": "cuda" if args.cuda and torch.cuda.is_available() else "cpu",
                            "seed": args.seed,
                            "optuna": args.optuna}
+
+        # W&B initialization
+
+        wandb.init(
+            project="sac_antv5",
+            config=self.parameters
+        )
 
         # experiment data directory
 
@@ -429,6 +437,10 @@ class NormalController:
                                      num_samples,
                                      average_return,
                                      real_time]
+
+            wandb.log(data={"Key Metrics/Average Return": average_return,
+                            "Real Time": real_time},
+                      step=num_time_steps)
 
             print(f"evaluation at {num_time_steps} time steps: {average_return}")
 

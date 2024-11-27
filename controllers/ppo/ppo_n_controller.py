4316c22ba3cddaf879f8a30afc1b578877d72916
Sheila Schoepp <sschoepp@ualberta.ca>
2024-11-27 11:19:20 -0700

    u

--- controllers/ppo/ppo_n_controller.py
+++ controllers/ppo/ppo_n_controller.py
@@ -898,33 +898,34 @@ def main():
             print(f"Trial {trial.number} completed. Total trials so far: {len(study.trials)}\n")
 
         # trial 201
-        study.enqueue_trial(
-            {"lr": 0.00058328,
-             "linear_lr_decay": True,
-             "gamma": 0.9785,
-             "num_samples": 2048,
-             "mini_batch_size": 512,
-             "num_epochs": 5,
-             "epsilon": 0.2885,
-             "vf_loss_coef": 0.4932,
-             "policy_entropy_coef": 0.0055157,
-             "gae_lambda": 0.9463,
-             "normalize_rewards": False},
-        )
-
         # study.enqueue_trial(
-        #     {"lr": 0.00016626,
+        #     {"lr": 0.00058328,
         #      "linear_lr_decay": True,
-        #      "gamma": 0.9627,
+        #      "gamma": 0.9785,
         #      "num_samples": 2048,
-        #      "mini_batch_size": 32,
-        #      "num_epochs": 3,
-        #      "epsilon": 0.2642,
-        #      "vf_loss_coef": 0.8955,
-        #      "policy_entropy_coef": 0.012144,
-        #      "gae_lambda": 0.9459,
+        #      "mini_batch_size": 512,
+        #      "num_epochs": 5,
+        #      "epsilon": 0.2885,
+        #      "vf_loss_coef": 0.4932,
+        #      "policy_entropy_coef": 0.0055157,
+        #      "gae_lambda": 0.9463,
         #      "normalize_rewards": False},
         # )
+
+        # trial 202
+        study.enqueue_trial(
+            {"lr": 0.00016626,
+             "linear_lr_decay": True,
+             "gamma": 0.9627,
+             "num_samples": 2048,
+             "mini_batch_size": 32,
+             "num_epochs": 3,
+             "epsilon": 0.2642,
+             "vf_loss_coef": 0.8955,
+             "policy_entropy_coef": 0.012144,
+             "gae_lambda": 0.9459,
+             "normalize_rewards": False},
+        )
 
         study.optimize(
             objective,

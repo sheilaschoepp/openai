c9bb891b010ae1f97ffa6e210e666b8bb0d7dac7
Sheila Schoepp <sschoepp@ualberta.ca>
2025-02-18 09:20:38 -0700

    u

--- scripts/ppo.sh
+++ scripts/ppo.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Define session range and maximum active sessions
-SESSION_START=421
-SESSION_END=450
+SESSION_START=0
+SESSION_END=200
 MAX_ACTIVE_SESSIONS=21
 
 # Define command to run in each session

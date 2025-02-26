c2524090b6452f44974dd4f4936dea2772789666
sschoepp <sschoepp@ualberta.ca>
2025-02-26 09:28:57 -0700

    updated __init__.py

--- custom_gym_envs/__init__.py
+++ custom_gym_envs/__init__.py
@@ -1,31 +1,31 @@
 from gymnasium.envs.registration import register
 
 register(
-    id="AntEnvF0-N",
+    id="AntEnv-F0",
     entry_point="custom_gym_envs.envs.ant.AntEnvF0_Normal:AntEnvF0",
     max_episode_steps=1000,
 )
 
 register(
-    id="AntEnvF1-AROM",
+    id="AntEnv-F1",
     entry_point="custom_gym_envs.envs.ant.AntEnvF1_Ankle4ROM:AntEnvF1",
     max_episode_steps=1000,
 )
 
 register(
-    id="AntEnvF2-HROM",
+    id="AntEnv-F2",
     entry_point="custom_gym_envs.envs.ant.AntEnvF2_Hip4ROM:AntEnvF2",
     max_episode_steps=1000,
 )
 
 register(
-    id="AntEnvF3-BSL",
+    id="AntEnv-F3",
     entry_point="custom_gym_envs.envs.ant.AntEnvF3_BrokenSeveredLimb:AntEnvF3",
     max_episode_steps=1000,
 )
 
 register(
-    id="AntEnvF4-BUL",
+    id="AntEnv-F4",
     entry_point="custom_gym_envs.envs.ant.AntEnvF4_BrokenUnseveredLimb:AntEnvF4",
     max_episode_steps=1000,
 )

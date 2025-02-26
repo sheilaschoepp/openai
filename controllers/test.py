50f3a109817c0c5850e7706d9ad6ed67a5f3ce78
sschoepp <sschoepp@ualberta.ca>
2025-02-26 12:51:35 -0700

    added best hyperparameter files

--- controllers/test.py
+++ controllers/test.py
@@ -3,18 +3,13 @@ import time
 
 import custom_gym_envs
 
-env = gym.make("AntEnv-F3-BSL", render_mode="human", camera_name="free")
+env = gym.make("AntEnv-F3", render_mode="human", camera_name="free")
 
-unwrapped_env = env.unwrapped
-
-# Get the correct body ID for "torso"
-torso_id = unwrapped_env.model.body(name="torso").id
-print(torso_id)
 
 for episode in range(1000):
     env.reset(seed=episode)
-    for step in range(500):
+    for step in range(300):
         env.step(env.action_space.sample())
-        time.sleep(0.01)
+        time.sleep(0.05)
 
 env.close()

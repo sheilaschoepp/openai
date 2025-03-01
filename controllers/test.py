d8e84884b66ebfbd5f117d7c3441eb3bd8ad3df4
sschoepp <sschoepp@ualberta.ca>
2025-03-01 16:23:53 -0700

    new env

--- controllers/test.py
+++ controllers/test.py
@@ -5,12 +5,12 @@ import gymnasium_robotics
 
 import custom_gym_envs
 
-env = gym.make("FetchReach-F0", render_mode="human")
+env = gym.make("FetchReach-F0", render_mode="human", width=1600, height=800)
 
 
 for episode in range(1000):
     env.reset(seed=episode)
-    for step in range(300):
+    for step in range(100):
         env.step(env.action_space.sample())
         time.sleep(0.05)
 

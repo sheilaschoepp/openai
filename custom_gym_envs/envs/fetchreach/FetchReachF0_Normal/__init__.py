d44f68fcc12ca0219e2117f80dd61605b7d8cf0b
Sheila Schoepp <sschoepp@ualberta.ca>
2025-03-01 13:27:43 -0700

    added normal fetch reach env default files

---
+++ custom_gym_envs/envs/fetchreach/FetchReachF0_Normal/__init__.py
@@ -0,0 +1,2 @@
+from gymnasium_robotics.envs.fetch.fetch_env import MujocoFetchEnv, MujocoPyFetchEnv
+# from custom_gym_envs.envs.fetchreach.FetchReachF0_Normal.fetch_env import MujocoFetchEnv, MujocoPyFetchEnv

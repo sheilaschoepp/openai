5c0961bfa8633c5a7a28e5f507829d36687b0063
Sheila Schoepp <sschoepp@ualberta.ca>
2025-03-02 10:23:27 -0700

    updated ant ppo script

--- controllers/test.py
+++ controllers/test.py
@@ -5,7 +5,7 @@ import time
 import custom_gym_envs
 
 # env = gym.make("FetchReach-F1", render_mode="human", width=1600, height=800)
-env = gym.make("FetchReach-F1")
+env = gym.make("FetchReachDense-v3")
 
 for episode in range(1000):
     env.reset(seed=episode)

647f9ee02efd1c4784df843a9ddb40532c703a67
Sheila Schoepp <sschoepp@ualberta.ca>
2025-03-02 09:46:25 -0700

    updated test

--- controllers/test.py
+++ controllers/test.py
@@ -1,18 +1,16 @@
 import gymnasium as gym
-import time
-
 import gymnasium_robotics
+import time
 
 import custom_gym_envs
 
-env = gym.make("FetchReach-F2", render_mode="human", width=1600, height=800)
-
+# env = gym.make("FetchReach-F1", render_mode="human", width=1600, height=800)
+env = gym.make("FetchReach-F1")
 
 for episode in range(1000):
     env.reset(seed=episode)
     for step in range(100):
         obs, _, _, _, _ = env.step(env.action_space.sample())
-        # print(obs["observation"])
         time.sleep(0.05)
 
 env.close()

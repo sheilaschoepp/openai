57a30ea48f77b40328848f2d3f77e91c5f1ed231
Sheila Schoepp <sschoepp@ualberta.ca>
2025-03-02 18:25:02 -0700

    added consistency check

--- controllers/ppo/ppo_n_controller.py
+++ controllers/ppo/ppo_n_controller.py
@@ -395,6 +395,16 @@ class NormalController:
                     self.rlg.rl_agent_message(f'save_model, {self.data_dir}, {self.rlg.num_steps()}')
                     self.evaluate_model(self.rlg.num_steps())
 
+                # consistency check: run the n_controller for '2x' steps
+                # and compare the evaluation output to the n_controller
+                # run for 'x' steps followed by the ab_controller for
+                # 'x' steps; the evaluation output should be the same
+                # (only if this code is uncommented and 'x' is set
+                # properly)
+                # x = 4000
+                # if self.rlg.num_steps() % x == 0:
+                #     break
+
             # learning complete
             if self.rlg.num_steps() == self.parameters["n_time_steps"]:
                 break

4e028ea04844ab79971034011f327730a05ffd10
Sheila Schoepp <sschoepp@ualberta.ca>
2025-03-04 07:52:53 -0700

    u

--- scripts/fetchreachdensev3_sac_optuna.sh
+++ scripts/fetchreachdensev3_sac_optuna.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define session range and maximum active sessions
-SESSION_START=51 # start at 1
-SESSION_END=400
+SESSION_START=1 # start at 1
+SESSION_END=50
 MAX_ACTIVE_SESSIONS=21
 
 # Define command to run in each session
-COMMAND="python controllers/sac/sac_n_controller.py -e FetchReach-F0 -t 10000 -tef 100 -o"
+COMMAND="python controllers/sac/sac_n_controller.py -e FetchReach-F0 -t 10000 -tef 1000 -o"
 
 # Function to get the count of active tmux sessions
 get_active_tmux_sessions() {

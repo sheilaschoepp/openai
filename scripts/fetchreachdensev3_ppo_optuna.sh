5cd9809fa7f01050b652a5d50ac0ae6bb97baba6
Sheila Schoepp <sschoepp@ualberta.ca>
2025-03-05 10:57:28 -0700

    u

--- scripts/fetchreachdensev3_ppo_optuna.sh
+++ scripts/fetchreachdensev3_ppo_optuna.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Define session range and maximum active sessions
-SESSION_START=51 # start at 1
-SESSION_END=400
+SESSION_START=1 # start at 1
+SESSION_END=50
 MAX_ACTIVE_SESSIONS=21
 
 # Define command to run in each session
-COMMAND="python controllers/ppo/ppo_n_controller.py -e FetchReach-F0 -t 57344 -tef 8192 -o"
+COMMAND="python controllers/ppo/ppo_n_controller.py -e FetchReach-F0 -t 51200 -tef 1024 -o"
 
 # Function to get the count of active tmux sessions
 get_active_tmux_sessions() {

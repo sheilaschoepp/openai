193df91b9525852a872d4592d6f61e7d605fd2b3
Sheila Schoepp <sschoepp@ualberta.ca>
2025-05-02 09:54:30 -0600

    u

--- scripts/antv5_sac_experiment_ab.sh
+++ scripts/antv5_sac_experiment_ab.sh
@@ -8,20 +8,19 @@ SCRIPTS=(
 # Argument sets (everything except the --file=... portion)
 BASE_ARGS_LIST=(
 # melco1
-# currently running PPO normal for 20M
-#  "--ab_env_name=Ant-F1 --ab_time_steps=12000000 --wandb"
-#  "--ab_env_name=Ant-F1 --ab_time_steps=12000000 --wandb --clear_replay_buffer"
-#  "--ab_env_name=Ant-F1 --ab_time_steps=12000000 --wandb --reinitialize_networks"
-#  "--ab_env_name=Ant-F1 --ab_time_steps=12000000 --wandb --reinitialize_networks --clear_replay_buffer"
-#  "--ab_env_name=Ant-F2 --ab_time_steps=12000000 --wandb"
-#  "--ab_env_name=Ant-F2 --ab_time_steps=12000000 --wandb --clear_replay_buffer"
+  "--ab_env_name=Ant-F1 --ab_time_steps=12000000 --wandb"
+  "--ab_env_name=Ant-F1 --ab_time_steps=12000000 --wandb --clear_replay_buffer"
+  "--ab_env_name=Ant-F1 --ab_time_steps=12000000 --wandb --reinitialize_networks"
+  "--ab_env_name=Ant-F1 --ab_time_steps=12000000 --wandb --reinitialize_networks --clear_replay_buffer"
+  "--ab_env_name=Ant-F2 --ab_time_steps=12000000 --wandb"
+  "--ab_env_name=Ant-F2 --ab_time_steps=12000000 --wandb --clear_replay_buffer"
 # melco2
-  "--ab_env_name=Ant-F2 --ab_time_steps=12000000 --wandb --reinitialize_networks"
-  "--ab_env_name=Ant-F2 --ab_time_steps=12000000 --wandb --reinitialize_networks --clear_replay_buffer"
-  "--ab_env_name=Ant-F3 --ab_time_steps=12000000 --wandb"
-  "--ab_env_name=Ant-F3 --ab_time_steps=12000000 --wandb --clear_replay_buffer"
-  "--ab_env_name=Ant-F3 --ab_time_steps=12000000 --wandb --reinitialize_networks"
-  "--ab_env_name=Ant-F3 --ab_time_steps=12000000 --wandb --reinitialize_networks --clear_replay_buffer"
+#  "--ab_env_name=Ant-F2 --ab_time_steps=12000000 --wandb --reinitialize_networks"
+#  "--ab_env_name=Ant-F2 --ab_time_steps=12000000 --wandb --reinitialize_networks --clear_replay_buffer"
+#  "--ab_env_name=Ant-F3 --ab_time_steps=12000000 --wandb"
+#  "--ab_env_name=Ant-F3 --ab_time_steps=12000000 --wandb --clear_replay_buffer"
+#  "--ab_env_name=Ant-F3 --ab_time_steps=12000000 --wandb --reinitialize_networks"
+#  "--ab_env_name=Ant-F3 --ab_time_steps=12000000 --wandb --reinitialize_networks --clear_replay_buffer"
 # ur3
 #  "--ab_env_name=Ant-F4 --ab_time_steps=5000000 --wandb"
 #  "--ab_env_name=Ant-F4 --ab_time_steps=5000000 --wandb --clear_replay_buffer"

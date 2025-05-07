09af169ee4383ace01bb77ca1d73e3481a10304c
Sheila Schoepp <sschoepp@ualberta.ca>
2025-05-07 11:32:24 -0600

    u

--- scripts/antv5_ppo_experiment_ab.sh
+++ scripts/antv5_ppo_experiment_ab.sh
@@ -8,37 +8,34 @@ SCRIPTS=(
 # Argument sets (everything except the --file=... portion)
 BASE_ARGS_LIST=(
 #  melco2
-#  "--ab_env_name=Ant-F1 --ab_time_steps=120000000 --wandb"
-#  "--ab_env_name=Ant-F1 --ab_time_steps=120000000 --wandb --clear_memory"
-#  "--ab_env_name=Ant-F1 --ab_time_steps=120000000 --wandb --reinitialize_networks"
-#  "--ab_env_name=Ant-F1 --ab_time_steps=120000000 --wandb --reinitialize_networks --clear_memory"
+  "--ab_env_name=Ant-F1 --ab_time_steps=80000000 --wandb"
+  "--ab_env_name=Ant-F1 --ab_time_steps=80000000 --wandb --clear_memory"
+  "--ab_env_name=Ant-F1 --ab_time_steps=80000000 --wandb --reinitialize_networks"
+  "--ab_env_name=Ant-F1 --ab_time_steps=80000000 --wandb --reinitialize_networks --clear_memory"
 #  ur3
-#  "--ab_env_name=Ant-F2 --ab_time_steps=120000000 --wandb"
-#  "--ab_env_name=Ant-F2 --ab_time_steps=120000000 --wandb --clear_memory"
-#  "--ab_env_name=Ant-F2 --ab_time_steps=120000000 --wandb --reinitialize_networks"
-#  "--ab_env_name=Ant-F2 --ab_time_steps=120000000 --wandb --reinitialize_networks --clear_memory"
+  "--ab_env_name=Ant-F2 --ab_time_steps=80000000 --wandb"
+  "--ab_env_name=Ant-F2 --ab_time_steps=80000000 --wandb --clear_memory"
+#  "--ab_env_name=Ant-F2 --ab_time_steps=80000000 --wandb --reinitialize_networks"
+#  "--ab_env_name=Ant-F2 --ab_time_steps=80000000 --wandb --reinitialize_networks --clear_memory"
+#  melco2
+#  "--ab_env_name=Ant-F3 --ab_time_steps=80000000 --wandb"
+#  "--ab_env_name=Ant-F3 --ab_time_steps=80000000 --wandb --clear_memory"
+#  "--ab_env_name=Ant-F3 --ab_time_steps=80000000 --wandb --reinitialize_networks"
+#  "--ab_env_name=Ant-F3 --ab_time_steps=80000000 --wandb --reinitialize_networks --clear_memory"
 #  ur3
-  "--ab_env_name=Ant-F3 --ab_time_steps=80000000 --wandb"
-  "--ab_env_name=Ant-F3 --ab_time_steps=80000000 --wandb --clear_memory"
-  "--ab_env_name=Ant-F3 --ab_time_steps=80000000 --wandb --reinitialize_networks"
-  "--ab_env_name=Ant-F3 --ab_time_steps=80000000 --wandb --reinitialize_networks --clear_memory"
-  "--ab_env_name=Ant-F3 --ab_time_steps=160000000 --wandb"
-  "--ab_env_name=Ant-F3 --ab_time_steps=160000000 --wandb --clear_memory"
-  "--ab_env_name=Ant-F3 --ab_time_steps=160000000 --wandb --reinitialize_networks"
-  "--ab_env_name=Ant-F3 --ab_time_steps=160000000 --wandb --reinitialize_networks --clear_memory"
-  "--ab_env_name=Ant-F3 --ab_time_steps=200000000 --wandb"
-  "--ab_env_name=Ant-F3 --ab_time_steps=200000000 --wandb --clear_memory"
-  "--ab_env_name=Ant-F3 --ab_time_steps=200000000 --wandb --reinitialize_networks"
-  "--ab_env_name=Ant-F3 --ab_time_steps=200000000 --wandb --reinitialize_networks --clear_memory"
+#  "--ab_env_name=Ant-F4 --ab_time_steps=80000000 --wandb"
+#  "--ab_env_name=Ant-F4 --ab_time_steps=80000000 --wandb --clear_memory"
+#  "--ab_env_name=Ant-F4 --ab_time_steps=80000000 --wandb --reinitialize_networks"
+#  "--ab_env_name=Ant-F4 --ab_time_steps=80000000 --wandb --reinitialize_networks --clear_memory"
 )
 
 # Base file path (without the /seed part)
-FILE_BASE="/home/sschoepp/Documents/openai/data/PPO_Ant-v5:40000000_lr:0.0001672_lrd:True_g:0.996_ns:4096_mbs:32_epo:5_eps:0.2458_c1:0.4853_c2:0.003953_cvf:False_mgn:0.5_gae:True_lam:0.9006_nr:True_hd:64_lstd:0.0_tef:400000_ee:10_d:cpu_wb"
+FILE_BASE="/home/sschoepp/Documents/openai/data/PPO_Ant-v5:20000000_lr:0.0001672_lrd:True_g:0.996_ns:4096_mbs:32_epo:5_eps:0.2458_c1:0.4853_c2:0.003953_cvf:False_mgn:0.5_gae:True_lam:0.9006_nr:True_hd:64_lstd:0.0_tef:400000_ee:10_d:cpu_wb"
 
 # Number of seeds to run for each script/arg set
 NUM_SEEDS=5
 # Maximum tmux sessions allowed
-MAX_SESSIONS=16
+MAX_SESSIONS=31
 
 # Iterate over all script–argument pairs
 for i in "${!SCRIPTS[@]}"; do
